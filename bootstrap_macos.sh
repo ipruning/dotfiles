54267c0e2541ae105b96cf77321567a73b95c618
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-03-02 09:48:20 +0000

    add color

--- bootstrap_macos.sh
+++ bootstrap_macos.sh
@@ -1,5 +1,11 @@
 #!/bin/zsh
 
+RED="$(tput setaf 1)"
+GREEN="$(tput setaf 2)"
+YELLOW="$(tput setaf 3)"
+BLUE="$(tput setaf 4)"
+NORMAL="$(tput sgr0)"
+
 # main_arm64
 function main_arm64 {
   echo "${BLUE}Installing dotfiles for arm64"
