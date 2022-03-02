6b14011deb3da5b4bccd237d95fcfa1a1d1e7f88
Alex <30626291+ipruning@users.noreply.github.com>
2022-03-02 23:53:33 +0000

    feat(sh): Add support for x86, remove p10k theme

--- config/shell/init.sh
+++ config/shell/init.sh
@@ -1,65 +1,59 @@
 #!/bin/bash
 
 #===============================================================================
+# 👇 _INIT_SH_LOADED
+#===============================================================================
+if [ -z "$_INIT_SH_LOADED" ]; then
+  _INIT_SH_LOADED=1
+else
+  return
+fi
+
+#===============================================================================
+# 👇 Check system architecture
+#===============================================================================
+# if [[ "$(sysctl -a | grep machdep.cpu.brand_string)" == *Apple* ]]; then
+#   echo test
+# fi
+
+SYSTEM_ARCH=$(uname -m)
+
+case $SYSTEM_ARCH in
+arm64*)
+  # echo "arm64"
+  ;;
+x86_64*)
+  # echo "x86_64"
+  ;;
+*)
+  echo "unknown: $(uname -m)"
+  ;;
+esac
+
+#===============================================================================
 # 👇 zprof
 #===============================================================================
 # zmodload zsh/zprof # 在 ~/.zshrc 的头部加上这个，加载 profile 模块
 
 #===============================================================================
-# 👇 _INIT_SH_LOADED
-#===============================================================================
-if [ -z "$_INIT_SH_LOADED" ]; then
-    _INIT_SH_LOADED=1
-else
-    return
-fi
-
-#===============================================================================
 # 👇 env
 #===============================================================================
 if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
-    # run script for interactive mode of bash/zsh
-    if [[ $- == *i* ]] && [ -z "$_INIT_SH_NOFUN" ]; then
-        if [ -f "$HOME/dotfiles/config/shell/zsh_env.sh" ]; then
-            . "$HOME/dotfiles/config/shell/zsh_env.sh"
-        fi
+  # run script for interactive mode of bash/zsh
+  if [[ $- == *i* ]] && [ -z "$_INIT_SH_NOFUN" ]; then
+    if [ -f "$HOME/dotfiles/config/shell/zsh_env.sh" ]; then
+      . "$HOME/dotfiles/config/shell/zsh_env.sh"
     fi
-fi
-
-#===============================================================================
-# 👇 functions
-#===============================================================================
-if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
-    # run script for interactive mode of bash/zsh
-    if [[ $- == *i* ]] && [ -z "$_INIT_SH_NOFUN" ]; then
-        if [ -f "$HOME/dotfiles/config/shell/zsh_functions.sh" ]; then
-            . "$HOME/dotfiles/config/shell/zsh_functions.sh"
-        fi
+    if [ -f "$HOME/dotfiles/config/shell/zsh_functions.sh" ]; then
+      . "$HOME/dotfiles/config/shell/zsh_functions.sh"
     fi
-fi
-
-#===============================================================================
-# 👇 aliases
-#===============================================================================
-if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
-    # run script for interactive mode of bash/zsh
-    if [[ $- == *i* ]] && [ -z "$_INIT_SH_NOFUN" ]; then
-        if [ -f "$HOME/dotfiles/config/shell/zsh_aliases.sh" ]; then
-            . "$HOME/dotfiles/config/shell/zsh_aliases.sh"
-        fi
+    if [ -f "$HOME/dotfiles/config/shell/zsh_aliases.sh" ]; then
+      . "$HOME/dotfiles/config/shell/zsh_aliases.sh"
     fi
-fi
-
-#===============================================================================
-# 👇 completion
-#===============================================================================
-if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
-    # run script for interactive mode of bash/zsh
-    if [[ $- == *i* ]] && [ -z "$_INIT_SH_NOFUN" ]; then
-        if [ -f "$HOME/dotfiles/config/shell/zsh_completion.sh" ]; then
-            . "$HOME/dotfiles/config/shell/zsh_completion.sh"
-        fi
+    if [ -f "$HOME/dotfiles/config/shell/zsh_completion.sh" ]; then
+      . "$HOME/dotfiles/config/shell/zsh_completion.sh"
     fi
+  fi
 fi
 
 #===============================================================================
