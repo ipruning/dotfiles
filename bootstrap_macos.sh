7dfc897f51e831a6e5db5746ee6dd2fe436a1899
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-03-02 07:05:57 +0000

    fix zprofile

--- bootstrap_macos.sh
+++ bootstrap_macos.sh
@@ -11,8 +11,7 @@ function main_arm64 {
   mv "$HOME"/.zshrc "$HOME"/.zshrc.bak && touch "$HOME"/.zshrc
   grep --fixed-strings "dotfiles/config/shell/init.sh" ~/.zshrc || echo "source $HOME/dotfiles/config/shell/init.sh" >> "$HOME"/.zshrc
   mv "$HOME"/.zprofile "$HOME"/.zprofile.bak && touch "$HOME"/.zprofile
-  echo "eval "$(/opt/homebrew/bin/brew shellenv)"" >> "$HOME"/.zprofile
-  echo "export PATH="$PATH:${HOME}/.local/bin"" >> "$HOME"/.zprofile
+  ln -sf "$HOME"/dotfiles/config/shell/macos/zprofile_arm.sh "$HOME"/.zprofile
 
   # install zsh plugins
   git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
