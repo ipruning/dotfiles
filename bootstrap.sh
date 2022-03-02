136e6c96572595fef88ce661eb495ae5c5654a8f
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-03-02 03:41:05 +0000

    feat(bootstrap): add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+RED="$(tput setaf 1)"
+GREEN="$(tput setaf 2)"
+YELLOW="$(tput setaf 3)"
+BLUE="$(tput setaf 4)"
+NORMAL="$(tput sgr0)"
+
+# main
+function main {
+  # init dotfiles
+  git clone https://github.com/Spehhhhh/dotfiles.git "$HOME"/dotfiles
+
+  # init
+  case "$OSTYPE" in
+    darwin*)  sh ./bootstrap_macos.sh ;;
+    linux*)   echo "TODO" ;;
+    msys*)    echo "TODO" ;;
+    *)        echo "unknown: $OSTYPE" ;;
+  esac
+}
+
+# Checking the environment
+if [ -d "$HOME"/dotfiles ]; then
+	echo "${YELLOW}You already have dotfiles installed."
+	echo "${GREEN}Please remove $HOME/dotfiles if you want to re-install."
+	exit
+fi
+
+echo "${RED}This will overwrite existing files in your home directory. Are you sure? (y/n) "
+read -r
+
+if [[ $REPLY =~ ^[Yy] ]]; then
+	echo "${BLUE}Cloning dotfiles...${NORMAL}"
+	main
+else
+	echo "";
+fi;
---
+++ bootstrap_macos.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# main_arm64
+function main_arm64 {
+  echo "Installing dotfiles for arm64"
+  # install oh-my-zsh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+  # install Homebrew
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  # install Homebrew packages
+  brew bundle --file="$HOME"/dotfiles/config/brew/Brewfile
+
+  # install npm packages
+  asdf install nodejs lts
+  asdf global nodejs lts
+  xargs npm install --global < "$HOME"/dotfiles/assets/npm/npm.txt
+
+  # install pipx packages
+  xargs pipx install < "$HOME"/dotfiles/assets/pipx/npm.txt
+
+  # install other packages
+  curl -sSL https://git.io/JcGER | bash # AutoCorrect
+
+  echo "source $HOME/dotfiles/config/shell/init.sh" >> "$HOME"/.zshrc
+  ln -sf "$HOME"/dotfiles/config/mackup/.mackup.cfg "$HOME"/.mackup.cfg
+  ln -sf "$HOME"/dotfiles/config/mackup/.mackup "$HOME"/.mackup
+  mackup restore
+
+  git clone https://github.com/gpakosz/.tmux.git "$HOME"/.tmux
+  ln -sf "$HOME"/.tmux/.tmux.conf "$HOME"/.tmux.conf
+}
+
+# init
+arch_check=$(/usr/bin/arch)
+case $arch_check in
+  arm64*)
+    main_arm64
+    echo "Done"
+  ;;
+  i386*)
+    echo "Please check the path to miniforge in init.sh"
+  ;;
+  *)
+    echo "unknown: $arch_check"
+  ;;
+esac
