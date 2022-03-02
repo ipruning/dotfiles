0dc3531435e7a58eeb5692f1a24e12807bbcb4f8
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-03-02 09:27:24 +0000

    fix

--- bootstrap_macos.sh
+++ bootstrap_macos.sh
@@ -2,19 +2,22 @@
 
 # main_arm64
 function main_arm64 {
-  echo "Installing dotfiles for arm64"
+  echo "${BLUE}Installing dotfiles for arm64"
   
   # install oh-my-zsh
+  echo "${BLUE}Installing oh-my-zsh"
   export CHSH=no
   export RUNZSH=no
   export KEEP_ZSHRC=yes
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended --keep-zshrc
 
   # install zsh
+  echo "${BLUE}Installing zsh"
   grep --fixed-strings "dotfiles/config/shell/init.sh" ~/.zshrc || mv "$HOME"/.zshrc "$HOME"/.zshrc.bak && touch "$HOME"/.zshrc && echo "source $HOME/dotfiles/config/shell/init.sh" >> "$HOME"/.zshrc
   mv "$HOME"/.zprofile "$HOME"/.zprofile.bak && touch "$HOME"/.zprofile && cp "$HOME"/dotfiles/config/shell/macos/zprofile.sh "$HOME"/.zprofile
   
   # install zsh plugins
+  echo "${BLUE}Installing zsh plugins"
   git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
   git clone https://github.com/paulirish/git-open.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-open
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -23,19 +26,23 @@ function main_arm64 {
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
   # install Homebrew
+  echo "${BLUE}Installing Homebrew"
   which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
   # install Homebrew packages
+  echo "${BLUE}Installing Homebrew packages"
   eval "$(/opt/homebrew/bin/brew shellenv)"
   source $HOME/.zshrc
   brew bundle --file="$HOME"/dotfiles/assets/brew/BrewfileDev
 
   # install mackup
+  echo "${BLUE}Installing mackup"
   ln -sf "$HOME"/dotfiles/config/mackup/.mackup.cfg "$HOME"/.mackup.cfg
   ln -sf "$HOME"/dotfiles/config/mackup/.mackup "$HOME"/.mackup
   mackup restore
 
   # install asdf
+  echo "${BLUE}Installing asdf"
   asdf plugin-add clojure https://github.com/asdf-community/asdf-clojure.git
   asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
   asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
@@ -46,19 +53,24 @@ function main_arm64 {
   asdf install
 
   # install npm packages
+  echo "${BLUE}Installing npm packages"
   xargs npm install --global < "$HOME"/dotfiles/assets/npm/npm.txt
 
   # install pipx packages
+  echo "${BLUE}Installing pipx packages"
   cat "$HOME"/dotfiles/assets/pipx/pipx.txt | xargs -n 1 pipx install
 
   # install other packages
+  echo "${BLUE}Installing other packages"
   curl -sSL https://git.io/JcGER | bash # AutoCorrect
 
   # install oh-my-tmux
+  echo "${BLUE}Installing oh-my-tmux"
   git clone https://github.com/gpakosz/.tmux.git "$HOME"/.tmux
   ln -sf "$HOME"/.tmux/.tmux.conf "$HOME"/.tmux.conf
 
   # install SpaceVim
+  echo "${BLUE}Installing SpaceVim"
   curl -sLf https://spacevim.org/install.sh | bash
 
   # install doom-emacs
