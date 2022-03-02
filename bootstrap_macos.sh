99ecafee1163821422500c835114f2908435ec8f
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-03-02 08:19:56 +0000

    fix

--- bootstrap_macos.sh
+++ bootstrap_macos.sh
@@ -8,11 +8,9 @@ function main_arm64 {
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
   # install zsh
-  mv "$HOME"/.zshrc "$HOME"/.zshrc.bak && touch "$HOME"/.zshrc
-  grep --fixed-strings "dotfiles/config/shell/init.sh" ~/.zshrc || echo "source $HOME/dotfiles/config/shell/init.sh" >> "$HOME"/.zshrc
-  mv "$HOME"/.zprofile "$HOME"/.zprofile.bak && touch "$HOME"/.zprofile
-  ln -sf "$HOME"/dotfiles/config/shell/macos/zprofile.sh "$HOME"/.zprofile
-
+  grep --fixed-strings "dotfiles/config/shell/init.sh" ~/.zshrc || mv "$HOME"/.zshrc "$HOME"/.zshrc.bak && touch "$HOME"/.zshrc && echo "source $HOME/dotfiles/config/shell/init.sh" >> "$HOME"/.zshrc
+  mv "$HOME"/.zprofile "$HOME"/.zprofile.bak && touch "$HOME"/.zprofile && cp "$HOME"/dotfiles/config/shell/macos/zprofile.sh "$HOME"/.zprofile
+  
   # install zsh plugins
   git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
   git clone https://github.com/paulirish/git-open.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-open
@@ -22,10 +20,10 @@ function main_arm64 {
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
   # install Homebrew
-  eval "$(/opt/homebrew/bin/brew shellenv)"
   which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
   # install Homebrew packages
+  eval "$(/opt/homebrew/bin/brew shellenv)"
   source $HOME/.zshrc
   brew bundle --file="$HOME"/dotfiles/assets/brew/BrewfileDev
 
@@ -35,12 +33,12 @@ function main_arm64 {
   mackup restore
 
   # install asdf
-  asdf plugin add clojure https://github.com/asdf-community/asdf-clojure.git
+  asdf plugin-add clojure https://github.com/asdf-community/asdf-clojure.git
   asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
-  asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+  asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
   asdf plugin-add php https://github.com/asdf-community/asdf-php.git
   asdf plugin-add python
-  asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+  asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
   asdf plugin-add rust https://github.com/asdf-community/asdf-rust.git
   asdf install
 
