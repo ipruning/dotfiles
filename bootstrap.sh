897191e220bdd5375b318a22deaa5930167f29ad
Alex <30626291+ipruning@users.noreply.github.com>
2022-03-02 23:34:57 +0000

    feat: Refactoring code and adding Raspberry Pi support

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,31 +8,43 @@ NORMAL="$(tput sgr0)"
 
 # main
 function main {
-  # init dotfiles
+  echo "${BLUE}Cloning dotfiles...${NORMAL}"
   git clone --depth 1 https://github.com/Spehhhhh/dotfiles.git "$HOME"/dotfiles
 
   # init
+  echo "${BLUE}Runing the bootstrap script...${NORMAL}"
   case "$OSTYPE" in
-    darwin*)  sh "$HOME"/dotfiles/bootstrap_macos.sh ;;
-    linux*)   echo "TODO" ;;
-    msys*)    echo "TODO" ;;
-    *)        echo "unknown: $OSTYPE" ;;
+  darwin*)
+    sh "$HOME"/dotfiles/bootstrap_macos.sh
+    ;;
+  linux*)
+    if [[ "$(uname -m)" == *armv7l* ]]; then
+      sh "$HOME"/dotfiles/bootstrap_raspberry.sh
+    else
+      echo "${RED}Unsupported system architecture.${NORMAL}"
+    fi
+    ;;
+  msys*)
+    echo "${RED}Unsupported system architecture.${NORMAL}"
+    ;;
+  *)
+    echo "${RED}unknown: $OSTYPE${NORMAL}"
+    ;;
   esac
 }
 
 # Checking the environment
 if [ -d "$HOME"/dotfiles ]; then
-	echo "${YELLOW}You already have dotfiles installed.${NORMAL}"
-	echo "${GREEN}Please remove $HOME/dotfiles if you want to re-install.${NORMAL}"
-	exit
+  echo "${YELLOW}You already have dotfiles installed.${NORMAL}"
+  echo "${GREEN}Please remove $HOME/dotfiles if you want to re-install.${NORMAL}"
+  exit
 fi
 
 echo "${RED}This will overwrite existing files in your home directory. Are you sure? (y/n)${NORMAL}"
 read -r
 
 if [[ $REPLY =~ ^[Yy] ]]; then
-	echo "${BLUE}Cloning dotfiles...${NORMAL}"
-	main
+  main
 else
-	echo "";
-fi;
+  echo ""
+fi
