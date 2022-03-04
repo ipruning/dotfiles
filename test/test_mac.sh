607ad96b03e776ca76967bcb079638880ca0e0a7
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-03-04 02:32:15 +0000

    fix

--- test/test_mac.sh
+++ test/test_mac.sh
@@ -7,6 +7,7 @@ echo "Testing for macOS"
 echo "Testing Homebrew"
 test -x "$(which brew)"
 test -x "$(which git)"
+test -x "$(which code)" # VSCode
 
 echo "Testing oh-my-zsh"
 test -x "$(which zsh)"
