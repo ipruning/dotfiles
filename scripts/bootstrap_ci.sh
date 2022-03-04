66eff015ae641d6460fba84b589b821c6bfc190a
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-03-04 09:53:11 +0000

    fix

--- scripts/bootstrap_ci.sh
+++ scripts/bootstrap_ci.sh
@@ -4,7 +4,6 @@ echo "${BLUE}Installing oh-my-zsh${NORMAL}"
 export CHSH=no
 export RUNZSH=no
 export KEEP_ZSHRC=yes
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended --keep-zshrc
 
 echo "${BLUE}Installing Homebrew${NORMAL}"
 which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -17,6 +16,7 @@ sudo sh -c "echo $ZSH_PATH >> /etc/shells"
 sudo chsh -s "$ZSH_PATH"
 
 echo "${BLUE}Installing zsh dotiles${NORMAL}"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended --keep-zshrc
 grep --fixed-strings "dotfiles/config/shell/init.sh" "$HOME"/.zshrc || mv "$HOME"/.zshrc "$HOME"/.zshrc.bak && cp "$HOME"/dotfiles/config/shell/mac/zshrc.sh "$HOME"/.zshrc
 mv "$HOME"/.zprofile "$HOME"/.zprofile.bak && cp "$HOME"/dotfiles/config/shell/mac/zprofile.sh "$HOME"/.zprofile
 
