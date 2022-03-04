19df0b464f6dfcebdd0fbfb0a286e009262be398
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-03-04 08:08:42 +0000

    fix

--- test/test_mac.sh
+++ test/test_mac.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 set -e
 
 test -d "$HOME"/dotfiles
@@ -6,7 +8,8 @@ echo "Testing"
 echo "Testing Git"
 test -x "$(which git)"
 echo "Testing Python"
-python --version | rg "Python 3.10.2"
+zsh -c "python --version" | grep --fixed-strings "Python 3.10.2"
+"python --version" | grep --fixed-strings "Python 3.10.2"
 which python | rg "shims"
 echo "Testing ZSH"
 test -x "$(which zsh)"
