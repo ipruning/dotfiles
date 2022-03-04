26d9c431300dc020d391816fafafdfb735aad7ed
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-03-04 11:14:56 +0000

    fix

--- test/test_ci.sh
+++ test/test_ci.sh
@@ -10,20 +10,23 @@ echo "Testing Git"
 test -x "$(which git)"
 
 case "$SYSTEM_OS" in
-mac*)
-  echo "Testing Homebrew"
-  test -x "$(which brew)"
-  python --version | grep --fixed-strings "Python 3.10.2"
-  "$(brew --prefix)"/bin/zsh -c "python --version" | grep --fixed-strings "Python 3.10.2"
-  which python | rg "shims"
+mac)
+  # python --version | grep --fixed-strings "Python 3.10.2"
+  # "$(brew --prefix)"/bin/zsh -c "python --version" | grep --fixed-strings "Python 3.10.2"
+  # which python | rg "shims"
+  # echo "Testing Mackup"
+
   ;;
 *)
   echo "unknown $SYSTEM_OS"
   ;;
 esac
 
-echo "Testing Mackup"
+echo "Testing Homebrew"
+test -x "$(which brew)"
+
 cat "$HOME"/.zshrc
 test -f "$HOME"/.zshrc
+
 cat "$HOME"/.zprofile
 test -f "$HOME"/.zprofile
