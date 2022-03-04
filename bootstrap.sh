e2a9b69008a03a8b1525da716452d96606c49a79
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-03-04 05:47:25 +0000

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,8 @@ NORMAL="$(tput sgr0)"
 
 # main
 function main {
+  echo "${BLUE}Cloning dotfiles...${NORMAL}"
+  git clone --depth 1 https://github.com/Spehhhhh/dotfiles.git "$HOME"/dotfiles
   echo "${BLUE}Runing the bootstrap script...${NORMAL}"
   source "$HOME"/dotfiles/bin/csys # check SYSTEM_OS, SYSTEM_ARCH
   case "$OSTYPE" in
@@ -33,22 +35,19 @@ function main {
 MODE="$1"
 
 if [[ $MODE == "--force" ]]; then
+  rm -rf "$HOME"/dotfiles
   main
 else
   echo "${RED}This will overwrite existing files in your home directory. Are you sure? (y/n)${NORMAL}"
   read -r
-  # Checking the environment
-  if [ -d "$HOME"/dotfiles ]; then
-    echo "${YELLOW}You already have dotfiles installed.${NORMAL}"
-    echo "${GREEN}Please remove $HOME/dotfiles if you want to re-install.${NORMAL}"
-    exit
-  fi
-  echo "${RED}This will overwrite existing files in your home directory. Are you sure? (y/n)${NORMAL}"
-  read -r
   if [[ $REPLY =~ ^[Yy] ]]; then
-    echo "${BLUE}Cloning dotfiles...${NORMAL}"
-    git clone --depth 1 https://github.com/Spehhhhh/dotfiles.git "$HOME"/dotfiles
-    main
+    if [ -d "$HOME"/dotfiles ]; then
+      echo "${YELLOW}You already have dotfiles installed.${NORMAL}"
+      echo "${GREEN}Please remove $HOME/dotfiles if you want to re-install.${NORMAL}"
+      exit
+    else
+      main
+    fi
   else
     echo ""
   fi
