3ade403e156905a8276513a1b7a2e473587aa55e
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-04-23 00:25:42 +0100

    fix

--- assets/mackup/.config/wezterm/wezterm.lua
+++ assets/mackup/.config/wezterm/wezterm.lua
@@ -1,5 +1,25 @@
 local wezterm = require 'wezterm';
 return {
     font = wezterm.font("CaskaydiaCove Nerd Font"),
-    color_scheme = "Snazzy"
+    font_size = 13.0,
+    color_scheme = "iceberg-dark",
+    -- default_cursor_style = "BlinkingBar",
+    -- cursor_blink_rate = 500,
+    -- force_reverse_video_cursor = true,
+    use_fancy_tab_bar = false,
+    hide_tab_bar_if_only_one_tab = true,
+    tab_bar_at_bottom = true,
+    window_padding = {
+        left = 35,
+        -- right = 0,
+        top = 10,
+        -- bottom = 0,
+    },
+    window_decorations = "RESIZE",
+    native_macos_fullscreen_mode = true,
+    -- window_background_opacity = 0.98,
+    -- keys = {
+    -- {key="Enter", mods="ALT", action="DisableDefaultAssignment"},
+    -- {key="f", mods="CTRL|SUPER", action="ToggleFullScreen"},
+    -- },
 }
