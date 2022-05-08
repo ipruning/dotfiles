0ae2e7008a7de682a9996d5eb107297d40e3f1bf
Alex <30626291+ipruning@users.noreply.github.com>
2022-05-08 05:04:13 +0100

    add zsh-vi-mode

--- config/shell/zsh_functions.sh
+++ config/shell/zsh_functions.sh
@@ -209,3 +209,16 @@ cd() {
         builtin cd "$dir" &> /dev/null
     done
 }
+
+#===============================================================================
+# 👇 https://github.com/xbin-io/xbin/
+#===============================================================================
+function xbin() {
+  command="$1"
+  args="${@:2}"
+  if [ -t 0 ]; then
+    curl -X POST "https://xbin.io/${command}" -H "X-Args: ${args}"
+  else
+    curl --data-binary @- "https://xbin.io/${command}" -H "X-Args: ${args}"
+  fi
+}
