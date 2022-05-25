00f18988c4a92e6e36e538d6f98c830a5172145c
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-05-25 11:40:23 +0100

    add Org-roam & change font

--- assets/mackup/.doom.d/config.el
+++ assets/mackup/.doom.d/config.el
@@ -19,8 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :name "CaskaydiaCove Nerd Font" :size 13)
+      doom-variable-pitch-font (font-spec :name "CaskaydiaCove Nerd Font" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -29,7 +29,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Database/Org/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
