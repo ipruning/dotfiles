fd85cb1a99355a217b69754f2b16a6a2ebf130de
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-06-03 02:06:27 +0100

    fix zsh SYSTEM_TYPE check

--- config/shell/mac/zprofile.sh
+++ config/shell/mac/zprofile.sh
@@ -1,17 +1,67 @@
-#!/usr/bin/env bash
+#===============================================================================
+# 👇 csys
+#===============================================================================
 
-# homebrew shell env
-case $(uname -m) in
-arm64*)
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+# if [[ "$(sysctl -a | grep machdep.cpu.brand_string)" == *Apple* ]]; then
+#   echo test
+# fi
+
+SYSTEM_ARCH=$(uname -m)
+
+case "$OSTYPE" in
+darwin*)
+  case $SYSTEM_ARCH in
+  arm64*)
+    SYSTEM_TYPE="mac_arm64"
+    ;;
+  x86_64*)
+    SYSTEM_TYPE="mac_x86_64"
+    ;;
+  *)
+    SYSTEM_TYPE="unknown"
+    ;;
+  esac
   ;;
-x86_64*)
-  eval "$(/usr/local/homebrew/bin/brew shellenv)"
+linux*)
+  case $SYSTEM_ARCH in
+  x86_64*)
+    SYSTEM_TYPE="linux_x86_64"
+    ;;
+  *armv7l*)
+    SYSTEM_TYPE="raspberry"
+    ;;
+  *)
+    SYSTEM_TYPE="unknown"
+    ;;
+  esac
+  ;;
+msys*)
+  SYSTEM_TYPE="unknown"
   ;;
 *)
-  echo "unknown: $(uname -m)"
+  SYSTEM_TYPE="unknown"
   ;;
 esac
 
-# homebrew & pipx & other binaries
+#===============================================================================
+# 👇 homebrew shell env
+#===============================================================================
+case $SYSTEM_TYPE in
+mac_arm64)
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  ;;
+mac_x86_64)
+  eval "$(/usr/local/homebrew/bin/brew shellenv)"
+  ;;
+linux_x86_64)
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  ;;
+unknown)
+  echo "${RED}Unsupported system architecture.${NORMAL}"
+  ;;
+esac
+
+#===============================================================================
+# 👇 homebrew & pipx & other binaries
+#===============================================================================
 export PATH="$PATH:${HOME}/.local/bin"
