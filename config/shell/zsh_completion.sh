01f20bcf48435047466dbef7e2fd94d79752bbc5
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-06-08 13:20:39 +0100

    fix completion

--- config/shell/zsh_completion.sh
+++ config/shell/zsh_completion.sh
@@ -1,5 +1,21 @@
+#===============================================================================
+# 👇 zsh-completions
+# if you are using a custom compinit setup with a ZSH Framework, ensure compinit is below your sourcing of the framework
+#===============================================================================
+autoload -Uz compinit && compinit
+
+#===============================================================================
+# 👇 completions
+# if you are using a custom compinit setup with a ZSH Framework, ensure compinit is below your sourcing of the framework
+#===============================================================================
 case $SYSTEM_TYPE in
 mac_arm64 | mac_x86_64)
+  #===============================================================================
+  # 👇 1password completions
+  #===============================================================================
+  eval "$(op completion zsh)"
+  compdef _op op
+
   #===============================================================================
   # 👇 cz completions
   #===============================================================================
@@ -16,11 +32,4 @@ mac_arm64 | mac_x86_64)
   eval "$(zoxide init zsh)"
   ;;
 raspberry) ;;
-
 esac
-
-#===============================================================================
-# 👇 zsh-completions
-# if you are using a custom compinit setup with a ZSH Framework, ensure compinit is below your sourcing of the framework
-#===============================================================================
-autoload -Uz compinit && compinit
