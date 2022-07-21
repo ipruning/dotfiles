f778d619b98bb6101dbcab9d63e7803eba15da03
Alex <30626291+ipruning@users.noreply.github.com>
2022-07-21 09:57:32 +0100

    add navi

--- config/shell/zsh_env.sh
+++ config/shell/zsh_env.sh
@@ -122,6 +122,8 @@ mac*)
     bindkey '≈' fzf-dirs-widget
     # 👇 Ctrl-L accept zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions#key-bindings
     bindkey '^L' autosuggest-accept
+    # 👇 Ctrl-G
+    bindkey '^g' _navi_widget
   }
   ;;
 linux*)
@@ -241,6 +243,45 @@ export BAT_THEME="OneHalfDark"
 export PATH="$(brew --prefix llvm)/bin:${PATH}"
 export LDFLAGS="-L$(brew --prefix llvm)/lib"
 export CPPFLAGS="-I$(brew --prefix llvm)/include"
+
+#===============================================================================
+# 👇 navi
+# 👇 eval "$(navi widget zsh)"
+#===============================================================================
+_navi_call() {
+  local result="$(navi "$@" </dev/tty)"
+  printf "%s" "$result"
+}
+
+_navi_widget() {
+  local -r input="${LBUFFER}"
+  local -r last_command="$(echo "${input}" | navi fn widget::last_command)"
+  local replacement="$last_command"
+
+  if [ -z "$last_command" ]; then
+    replacement="$(_navi_call --print)"
+  elif [ "$LASTWIDGET" = "_navi_widget" ] && [ "$input" = "$previous_output" ]; then
+    replacement="$(_navi_call --print --query "$last_command")"
+  else
+    replacement="$(_navi_call --print --best-match --query "$last_command")"
+  fi
+
+  if [ -n "$replacement" ]; then
+    local -r find="${last_command}_NAVIEND"
+    previous_output="${input}_NAVIEND"
+    previous_output="${previous_output//$find/$replacement}"
+  else
+    previous_output="$input"
+  fi
+
+  zle kill-whole-line
+  LBUFFER="${previous_output}"
+  region_highlight=("P0 100 bold")
+  zle redisplay
+}
+
+zle -N _navi_widget
+# bindkey '^g' _navi_widget
 
 #===============================================================================
 # 👇 zoxide
