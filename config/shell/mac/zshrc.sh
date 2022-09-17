cd9b46d74dd0c060f13f847412575847992af3e5
Alex <30626291+ipruning@users.noreply.github.com>
2022-09-17 07:30:24 +0100

    fix

--- config/shell/mac/zshrc.sh
+++ config/shell/mac/zshrc.sh
@@ -1,46 +1,4 @@
 #===============================================================================
-# TODO
-#===============================================================================
-SYSTEM_ARCH=$(uname -m)
-
-case "$OSTYPE" in
-darwin*)
-  case $SYSTEM_ARCH in
-  arm64*)
-    SYSTEM_TYPE="mac_arm64"
-    ;;
-  x86_64*)
-    SYSTEM_TYPE="mac_x86_64"
-    ;;
-  *)
-    SYSTEM_TYPE="unknown"
-    ;;
-  esac
-  ;;
-linux*)
-  case $SYSTEM_ARCH in
-  x86_64*)
-    SYSTEM_TYPE="linux_x86_64"
-    ;;
-  *armv7l*)
-    SYSTEM_TYPE="raspberry"
-    ;;
-  *)
-    SYSTEM_TYPE="unknown"
-    ;;
-  esac
-  ;;
-msys*)
-  SYSTEM_TYPE="unknown"
-  ;;
-*)
-  SYSTEM_TYPE="unknown"
-  ;;
-esac
-
-export SYSTEM_TYPE
-
-#===============================================================================
 # 👇 init
 #===============================================================================
 source "$HOME"/dotfiles/config/shell/init.sh
