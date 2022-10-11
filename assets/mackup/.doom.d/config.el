6b1655f4ac3a3fc04f92925e86865f90fa09f011
Alex <30626291+ipruning@users.noreply.github.com>
2022-10-11 01:52:02 +0100

    database -> databases

--- assets/mackup/.doom.d/config.el
+++ assets/mackup/.doom.d/config.el
@@ -29,9 +29,9 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/Database/Org/")
+(setq org-directory "~/Databases/Org/")
 
-(setq org-roam-directory "~/Database/Org/")
+(setq org-roam-directory "~/Databases/Org/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
