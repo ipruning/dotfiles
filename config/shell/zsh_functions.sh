53cf58336102d29e7c00ec0140350daaccd07c7e
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-12-07 21:09:18 +0000

    fix xbin

--- config/shell/zsh_functions.sh
+++ config/shell/zsh_functions.sh
@@ -189,10 +189,11 @@ cd() {
 #===============================================================================
 function xbin() {
   command="$1"
-  args="${@:2}"
+  shift
+  args=( "$@" )
   if [ -t 0 ]; then
-    curl -X POST "https://xbin.io/${command}" -H "X-Args: ${args}"
+    curl -sS -X POST "https://xbin.io/${command}" -H "X-Args: ${args}"
   else
-    curl --data-binary @- "https://xbin.io/${command}" -H "X-Args: ${args}"
+    curl -sS --data-binary @- "https://xbin.io/${command}" -H "X-Args: ${args}"
   fi
 }
