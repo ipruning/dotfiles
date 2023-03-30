fc8883bd71d32fce5061619d634d5aa44b7f01af
Alex <30626291+Spehhhhh@users.noreply.github.com>
2023-03-30 01:29:53 +0100

    feat: Add new functions for efficient data analysis.
    - Add new data analysis functions `hgpt` and `dgpt` to `config/shell/zsh_functions.sh`

--- config/shell/zsh_functions.sh
+++ config/shell/zsh_functions.sh
@@ -197,3 +197,22 @@ function xbin() {
     curl -sS --data-binary @- "https://xbin.io/${command}" -H "X-Args: ${args}"
   fi
 }
+
+#===============================================================================
+# 👇 https://kadekillary.work/posts/1000x-eng/
+# Examples of efficient data analysis using shell scripts
+# hgpt "create a 10 row csv of NBA player data with headers - please only include the data, nothing else" > nba.csv
+# dgpt "can you write a sql query to get the average PointsPerGame by Position from the following" "$(cat nba.csv)"
+#===============================================================================
+function hgpt {
+    local prompt=$1
+
+    ai "$prompt"
+}
+function dgpt() {
+    local prompt=$1
+    local data=$2
+    local prompt=$(echo "${prompt}: ${data}" | tr -s ' ')
+
+    ai "$prompt"
+}
