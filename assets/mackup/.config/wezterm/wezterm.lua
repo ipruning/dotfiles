791e2112700150adc801f58e1386776e7a85be79
Alex <30626291+ipruning@users.noreply.github.com>
2023-12-07 21:30:47 +0800

    chore(cursor,git): update settings and config

--- assets/mackup/.config/wezterm/wezterm.lua
+++ assets/mackup/.config/wezterm/wezterm.lua
@@ -1,8 +1,8 @@
 local wezterm = require 'wezterm';
 return {
-    font = wezterm.font("CaskaydiaCove Nerd Font"),
+    font = wezterm.font("MonoLisa Nerd Font"),
     font_size = 14.0,
-    color_scheme = "nord",
+    color_scheme = "Catppuccin Mocha",
     -- default_cursor_style = "BlinkingBar",
     -- cursor_blink_rate = 500,
     -- force_reverse_video_cursor = true,
@@ -10,14 +10,14 @@ return {
     hide_tab_bar_if_only_one_tab = true,
     tab_bar_at_bottom = true,
     window_padding = {
-        left = 35,
+        left = 20,
         -- right = 0,
-        top = 10,
+        top = 20,
         -- bottom = 0,
     },
     window_decorations = "RESIZE",
     native_macos_fullscreen_mode = true,
-    -- window_background_opacity = 0.98,
+    -- window_background_opacity = 0.97,
     -- keys = {
     -- {key="Enter", mods="ALT", action="DisableDefaultAssignment"},
     -- {key="f", mods="CTRL|SUPER", action="ToggleFullScreen"},
