b81ddf51280807e30fd4447dc9d90cb08732a677
Alex <30626291+ipruning@users.noreply.github.com>
2023-12-07 21:54:58 +0800

    chore: refactor and streamline aliases and functions
    - Add two aliases: `cpi` and `mvi`
    - Add an alias for the `cat` command using `bat`
    - Remove the alias for `cpi`
    - Add an alias for `l` using `lsd`
    - Add an alias for `l` using `lsd` if connected via SSH
    - Remove the alias for `mvi`
    - Add an alias for `v` using `nvim`
    - Add two functions: `g-i` and `g-sync`
    - Remove the alias for `br`
    - Add an alias for `cwd` that copies the current working directory to the clipboard
    - Add aliases for various tools and applications
    - Remove the aliases for `r-p2t` and `r-unm`
    - Add three functions: `r-update`, `r-upgrade`, and `r-backup`

--- config/shell/zsh_aliases.sh
+++ config/shell/zsh_aliases.sh
@@ -13,15 +13,21 @@ esac
 #===============================================================================
 # 👇
 #===============================================================================
+alias cpi='cp -i'
+alias mvi='mv -i'
+
+#===============================================================================
+# 👇
+#===============================================================================
 alias cat='bat --paging=never'
 # alias cp='fcp'
-alias cpi='cp -i'
 # alias cut='choose'
 # alias df='duf'
 # alias dig='dog'
 # alias du='dust'
 # alias find='fd'
 # alias grep='rg'
+# alias br='br -s'
 alias l='lsd'
 alias la='lsd -a'
 alias ll='lsd -lh'
@@ -30,7 +36,6 @@ alias lt='lsd --tree'
 if [[ -n $SSH_CONNECTION ]]; then
   alias lsd='lsd --icon never'
 fi
-alias mvi='mv -i'
 # alias ping='gping'
 # alias ps='procs'
 # alias rip='rip -i'
@@ -38,27 +43,31 @@ alias rmi='rm -i'
 # alias sed="sd"
 # alias sort="huniq"
 # alias top='htop'
+# alias vim='nvim'
 alias v='nvim'
-# alias ai='ai'
-# alias chatgpt='chatgpt'
-# alias copilot='copilot'
 # alias gptcommit='gptcommit'
 
 #===============================================================================
 # 👇
 #===============================================================================
-alias cwd='printf "%q\n" "$(pwd)" | pbcopy' # Copy current working directory to clipboard
+alias cwd='printf "%q\n" "$(pwd)" | pbcopy'
 alias ehost='${=EDITOR} /etc/hosts'
 alias eohmyzsh='${=EDITOR} ~/.oh-my-zsh'
 alias ezshrc='${=EDITOR} ~/.zshrc'
 alias szshrc='source ~/.zshrc'
-alias gii='git init && git commit --allow-empty -m "init"' # Initialize a git repo and make an empty commit
+alias gii='git init && git commit --allow-empty -m "init"'
 alias ip='curl -4 ip.sb'
 alias ipv6='curl -6 ip.sb'
 
-#===============================================================================
-# 👇 run
-#===============================================================================
+g-i() {
+  git init
+  git commit --allow-empty -m "init"
+}
+
+g-sync() {
+  gh repo list --fork --visibility public --json owner,name | jq -r 'map(.owner.login + "/" + .name) | .[]' | xargs -t -L1 gh repo sync
+}
+
 alias r-archivebox='cd /Volumes/Workspace/Databases/ArchiveBox && archivebox server'
 alias r-bb='/Applications/OpenBB\ Terminal/OpenBB\ Terminal'
 alias r-citespace='cd ${HOME}/Databases/CiteSpace/5.8.R3/ && ./StartCiteSpace_M1_Pro.sh'
@@ -67,12 +76,9 @@ alias r-fava='fava ${HOME}/Databases/Ledger/main.bean -p 4000'
 alias r-jupyter-lab='${HOME}/.conda/envs/LearningAI/bin/jupyter-lab'
 alias r-jupyter='${HOME}/.conda/envs/LearningAI/bin/jupyter-notebook'
 alias r-lol='open /Applications/League\ of\ Legends.app/ --args --locale=zh_CN'
-alias r-p2t='${HOME}/Stacks/Utilities/Pix2Text/.venv/bin/python ${HOME}/Stacks/Utilities/Pix2Text/scripts/screenshot_daemon.py'
-alias r-unm='node ${HOME}/Stacks/Utilities/UnblockNeteaseMusic/app.js -p 80:443 -f 103.126.92.132'
+alias r-p2t='${HOME}/Databases/Stacks/Utilities/Pix2Text/.venv/bin/python ${HOME}/Databases/Stacks/Utilities/Pix2Text/scripts/screenshot_daemon.py'
+alias r-unm='node ${HOME}/Databases/Stacks/Utilities/UnblockNeteaseMusic/app.js -p 80:443 -f 103.126.92.132'
 
-#===============================================================================
-# 👇 run update
-#===============================================================================
 r-update() {
   asdf latest --all
   brew cu
@@ -80,9 +86,6 @@ r-update() {
   tldr --update
 }
 
-#===============================================================================
-# 👇 run upgrade
-#===============================================================================
 r-upgrade() {
   asdf plugin update --all
   asdf update
@@ -91,7 +94,7 @@ r-upgrade() {
   # conda update --all
   gh extension upgrade --all
   juliaup update
-  mas upgrade
+  # mas upgrade
   npx npm-check --global --update-all
   omz update
   python -m pip install --upgrade pip
@@ -99,4 +102,17 @@ r-upgrade() {
   rustup self update
   rustup update
   wget https://raw.githubusercontent.com/reorx/ai.py/master/ai.py -O "${HOME}"/Dotfiles/bin/ai && chmod +x "${HOME}"/Dotfiles/bin/ai
+}
+
+r-backup() {
+  brew update
+  brew bundle dump --file="$HOME"/dotfiles/assets/others/packages/Brewfile --force
+  brew leaves >"$HOME"/dotfiles/assets/others/packages/Brewfile.txt
+  cargo install --list | grep -v '^[[:blank:]]' | awk '{print $1}' >"$HOME"/dotfiles/assets/others/packages/cargo.txt
+  ls /Applications | rg '\.app' | sed 's/\.app//g' >"$HOME"/dotfiles/assets/others/packages/macos_applications.txt
+  ls /Applications/Setapp | rg '\.app' | sed 's/\.app//g' >"$HOME"/dotfiles/assets/others/packages/macos_setapp.txt
+  npm list --location=global --json | jq ".dependencies | keys[]" -r >"$HOME"/dotfiles/assets/others/packages/npm.txt
+  pipx list --json | jq ".venvs | .[] | .metadata.main_package.package" -r >"$HOME"/dotfiles/assets/others/packages/pipx.txt
+  code --list-extensions >"$HOME"/dotfiles/assets/others/packages/vscode_extensions.txt
+  cp "$HOME"/.zsh_history "$HOME"/Databases/Backup/CLI/zsh_history_$(date +\%Y_\%m_\%d_\%H_\%M_\%S).bak
 }
