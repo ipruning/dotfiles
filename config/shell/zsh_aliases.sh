abecc857b827fddb2aff2addb56a2d19cd6832e7
Alex <30626291+ipruning@users.noreply.github.com>
2024-04-29 11:47:11 +0800

    feat: improve package management tool updates
    - Add update messages for package management tools
    - Add a new function for upgrading various tools
    - Remove outdated comment lines
    - Backup function now includes a message to update Homebrew before backup

--- config/shell/zsh_aliases.sh
+++ config/shell/zsh_aliases.sh
@@ -80,39 +80,87 @@ alias r-p2t='${HOME}/Databases/Stacks/Utilities/Pix2Text/.venv/bin/python ${HOME
 alias r-unm='node ${HOME}/Databases/Stacks/Utilities/UnblockNeteaseMusic/app.js -p 80:443 -f 103.126.92.132'
 
 r-update() {
+  echo -e "\033[33mUpdating all packages using asdf...\033[0m"
   asdf latest --all
+
+  echo -e "\033[33mChecking for outdated Homebrew formulas...\033[0m"
   brew cu
+
+  echo -e "\033[33mUpdating Homebrew...\033[0m"
   brew update
+
+  echo -e "\033[33mUpdating tldr pages...\033[0m"
   tldr --update
 }
 
+# mas upgrade
+# conda update --all
+
 r-upgrade() {
+  echo -e "\033[33mUpdating all asdf plugins...\033[0m"
   asdf plugin update --all
+
+  echo -e "\033[33mUpdating asdf...\033[0m"
   asdf update
+
+  echo -e "\033[33mUpgrading all Homebrew formulas...\033[0m"
   brew upgrade
+
+  echo -e "\033[33mUpdating all Rust crates...\033[0m"
   cargo install-update --all
-  # conda update --all
+
+  echo -e "\033[33mUpgrading all GitHub CLI extensions...\033[0m"
   gh extension upgrade --all
+
+  echo -e "\033[33mUpdating Julia...\033[0m"
   juliaup update
-  # mas upgrade
+
+  echo -e "\033[33mChecking and updating global npm packages...\033[0m"
   npx npm-check --global --update-all
+
+  echo -e "\033[33mUpdating Oh My Zsh...\033[0m"
   omz update
+
+  echo -e "\033[33mUpgrading Python pip and all pipx packages...\033[0m"
   python -m pip install --upgrade pip
   pipx upgrade-all
+
+  echo -e "\033[33mUpdating Rust toolchain...\033[0m"
   rustup self update
   rustup update
+
+  echo -e "\033[33mDownloading and setting permissions for ai.py script...\033[0m"
   wget https://raw.githubusercontent.com/reorx/ai.py/master/ai.py -O "${HOME}"/Dotfiles/bin/ai && chmod +x "${HOME}"/Dotfiles/bin/ai
 }
 
 r-backup() {
+  echo -e "\033[33mUpdating Homebrew before backup...\033[0m"
   brew update
+
+  echo -e "\033[33mDumping Homebrew bundle...\033[0m"
   brew bundle dump --file="$HOME"/dotfiles/assets/others/packages/Brewfile --force
+
+  echo -e "\033[33mCreating list of installed Homebrew leaves...\033[0m"
   brew leaves >"$HOME"/dotfiles/assets/others/packages/Brewfile.txt
+
+  echo -e "\033[33mListing installed Cargo packages...\033[0m"
   cargo install --list | grep -v '^[[:blank:]]' | awk '{print $1}' >"$HOME"/dotfiles/assets/others/packages/cargo.txt
+
+  echo -e "\033[33mListing installed Mac applications...\033[0m"
   ls /Applications | rg '\.app' | sed 's/\.app//g' >"$HOME"/dotfiles/assets/others/packages/macos_applications.txt
+
+  echo -e "\033[33mListing installed Setapp applications...\033[0m"
   ls /Applications/Setapp | rg '\.app' | sed 's/\.app//g' >"$HOME"/dotfiles/assets/others/packages/macos_setapp.txt
+
+  echo -e "\033[33mCreating npm global packages list...\033[0m"
   npm list --location=global --json | jq ".dependencies | keys[]" -r >"$HOME"/dotfiles/assets/others/packages/npm.txt
+
+  echo -e "\033[33mListing pipx managed packages...\033[0m"
   pipx list --json | jq ".venvs | .[] | .metadata.main_package.package" -r >"$HOME"/dotfiles/assets/others/packages/pipx.txt
+
+  echo -e "\033[33mListing Visual Studio Code extensions...\033[0m"
   code --list-extensions >"$HOME"/dotfiles/assets/others/packages/vscode_extensions.txt
+
+  echo -e "\033[33mBacking up .zsh_history...\033[0m"
   cp "$HOME"/.zsh_history "$HOME"/Databases/Backup/CLI/zsh_history_$(date +\%Y_\%m_\%d_\%H_\%M_\%S).bak
 }
