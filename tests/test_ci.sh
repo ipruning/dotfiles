bac17117ec956ab888524646a9739af098226c9c
Alex <30626291+ipruning@users.noreply.github.com>
2024-08-14 18:08:11 +0800

    test: refactor ZSH and Mackup testing workflows
    - Add echo statements for testing Go, ZSH, Homebrew, and Mackup in `tests/test_ci.sh`
    - Verify existence of `.zprofile` in `tests/test_ci.sh`
    - Verify existence of `.zshrc` in `tests/test_ci.sh`
    - Add `cat` commands for `.zshrc` and `.zprofile` in `tests/test_mac.sh`
    - Change sequence of checks and commands for testing ZSH and Mackup in `tests/test_mac.sh`
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- tests/test_ci.sh
+++ tests/test_ci.sh
@@ -4,3 +4,18 @@ set -e
 
 echo "Testing Git"
 test -x "$(which git)"
+
+echo "Testing Go"
+test -x "$(which go version)"
+
+echo "Testing ZSH"
+test -x "$(which zsh)"
+
+test -d "$HOME"/.oh-my-zsh
+
+echo "Testing Homebrew"
+test -x "$(which brew)"
+
+echo "Testing Mackup"
+test -f "$HOME"/.zprofile
+test -f "$HOME"/.zshrc
