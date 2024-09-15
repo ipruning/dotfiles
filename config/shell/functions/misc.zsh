ecdbf8064c8da3f15353176d6468e45799cf3271
Alex <30626291+ipruning@users.noreply.github.com>
2024-09-16 00:42:16 +0800

    refactor: refactor shell scripts and streamline workflow functions
    - Remove bash shebang from misc.zsh script
    - Remove Git section and related functions
    - Remove fzf directory jump widget function
    - Reformat `fcb` function to improve readability
    - Reformat `fkill` function to improve readability
    - Remove tmux section and related functions
    - Restore and enable cd function for interactive directory navigation
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- config/shell/functions/misc.zsh
+++ config/shell/functions/misc.zsh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 #===============================================================================
 # 👇 1Password
 #===============================================================================
@@ -28,40 +26,15 @@ _sudo-command-line() {
   [[ -z $BUFFER ]] && zle up-history
   local cmd="sudo "
   if [[ ${BUFFER} == ${cmd}* ]]; then
-    CURSOR=$(( CURSOR-${#cmd} ))
+    CURSOR=$((CURSOR - ${#cmd}))
     BUFFER="${BUFFER#"$cmd"}"
   else
     BUFFER="${cmd}${BUFFER}"
-    CURSOR=$(( CURSOR+${#cmd} ))
+    CURSOR=$((CURSOR + ${#cmd}))
   fi
   zle reset-prompt
 }
 zle -N _sudo-command-line
-
-#===============================================================================
-# 👇 Git
-#===============================================================================
-getrepo() {
-  git clone "$1" && cd "$(basename "$1" .git)" || exit
-}
-
-#===============================================================================
-# 👇 fzf Option-X 跳转近期目录
-#===============================================================================
-_fzf-dirs-widget() {
-  dir=$(dirs -v | fzf --height "${FZF_TMUX_HEIGHT:-40%}" --reverse | cut -b3-)
-  local dir
-  if [[ -z "$dir" ]]; then
-    zle redisplay
-    return 0
-  fi
-  eval cd "${dir}"
-  local ret=$?
-  unset dir # ensure this doesn't end up appearing in prompt expansion
-  zle reset-prompt
-  return $ret
-}
-zle -N _fzf-dirs-widget
 
 #===============================================================================
 # 👇 fzf completion will use == as the trigger sequence instead of the default **
@@ -73,10 +46,10 @@ _fzf_comprun() {
   local command=$1
   shift
   case "$command" in
-    cd)           fzf "$@" --preview 'tree -C {} | head -200' ;;
-    export|unset) fzf "$@" --preview "eval 'echo \$'{}" ;;
-    ssh)          fzf "$@" --preview 'dig {}' ;;
-    *)            fzf "$@" ;;
+  cd) fzf "$@" --preview 'tree -C {} | head -200' ;;
+  export | unset) fzf "$@" --preview "eval 'echo \$'{}" ;;
+  ssh) fzf "$@" --preview 'dig {}' ;;
+  *) fzf "$@" ;;
   esac
 }
 
@@ -85,7 +58,7 @@ _fzf_comprun() {
 #===============================================================================
 fch() {
   local cols sep google_history open
-  cols=$(( COLUMNS / 3 ))
+  cols=$((COLUMNS / 3))
   sep='{::}'
 
   if [ "$(uname)" = "Darwin" ]; then
@@ -99,57 +72,37 @@ fch() {
   sqlite3 -separator $sep /tmp/h \
     "select substr(title, 1, $cols), url
     from urls order by last_visit_time desc" |
-  awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
-  fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs $open > /dev/null 2> /dev/null
+    awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
+    fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs $open >/dev/null 2>/dev/null
 }
 
 #===============================================================================
 # 👇 fzf Chrome 书签
 #===============================================================================
 fcb() {
-    bookmarks_path=~/Library/Application\ Support/Google/Chrome/Default/Bookmarks
-    jq_script='
+  bookmarks_path=~/Library/Application\ Support/Google/Chrome/Default/Bookmarks
+  jq_script='
         def ancestors: while(. | length >= 2; del(.[-1,-2]));
         . as $in | paths(.url?) as $key | $in | getpath($key) | {name,url, path: [$key[0:-2] | ancestors as $a | $in | getpath($a) | .name?] | reverse | join("/") } | .path + "/" + .name + "\t" + .url'
-    jq -r "$jq_script" < "$bookmarks_path" \
-        | sed -E $'s/(.*)\t(.*)/\\1\t\x1b[36m\\2\x1b[m/g' \
-        | fzf --ansi \
-        | cut -d$'\t' -f2 \
-        | xargs open
+  jq -r "$jq_script" <"$bookmarks_path" |
+    sed -E $'s/(.*)\t(.*)/\\1\t\x1b[36m\\2\x1b[m/g' |
+    fzf --ansi |
+    cut -d$'\t' -f2 |
+    xargs open
 }
 
 #===============================================================================
 # 👇 fzf 杀进程
 #===============================================================================
 fkill() {
-  (date; ps -ef) |
+  (
+    date
+    ps -ef
+  ) |
     fzf --bind='ctrl-r:reload(date; ps -ef)' \
-        --header=$'Press CTRL-R to reload\n\n' --header-lines=2 \
-        --preview='echo {}' --preview-window=down,3,wrap \
-        --layout=reverse --height=80% | awk '{print $2}' | xargs kill -9
-}
-
-#===============================================================================
-# 👇 tmux
-#===============================================================================
-tm() {
-  [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
-  if [ "$1" ]; then
-    tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
-  fi
-  session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) && tmux $change -t "$session" || echo "No sessions found."
-}
-tmkill() {
-    local sessions
-    sessions="$(tmux ls|fzf --exit-0 --multi)"  || return $?
-    local i
-    for i in "${(f@)sessions}"
-    do
-        [[ $i =~ '([^:]*):.*' ]] && {
-            echo "Killing $match[1]"
-            tmux kill-session -t "$match[1]"
-        }
-    done
+      --header=$'Press CTRL-R to reload\n\n' --header-lines=2 \
+      --preview='echo {}' --preview-window=down,3,wrap \
+      --layout=reverse --height=80% | awk '{print $2}' | xargs kill -9
 }
 
 #===============================================================================
@@ -158,97 +111,83 @@ tmkill() {
 # TODO If an argument is provided, try to attach to the session with that name.
 # TODO With tab completion, this can be used to quickly switch to a session.
 zj() {
-    session=$(zellij list-sessions --no-formatting | awk '{
+  session=$(zellij list-sessions --no-formatting | awk '{
         session_name=$1; $1="";
         if ($0 ~ /EXITED/) print "\033[31m" session_name "\033[0m\t" $0;  # Red for exited sessions
         else print "\033[32m" session_name "\033[0m\t" $0;  # Green for active sessions
     }' | column -t -s $'\t' | fzf --ansi --exit-0 --header="Select a session to attach:" | awk '{print $1}')
 
-    if [ -n "$session" ]; then
-        zellij attach "$session"
-    else
-        echo "No sessions found."
-    fi
+  if [ -n "$session" ]; then
+    zellij attach "$session"
+  else
+    echo "No sessions found."
+  fi
 }
 
 #===============================================================================
 # 👇 cd
 #===============================================================================
-# cd() {
-#     if [[ "$#" != 0 ]]; then
-#         builtin cd "$@";
-#         return
-#     fi
-#     while true; do
-#         local lsd=$(echo ".." && ls -p | grep '/$' | sed 's;/$;;')
-#         local dir="$(printf '%s\n' "${lsd[@]}" |
-#             fzf --reverse --preview '
-#                 __cd_nxt="$(echo {})";
-#                 __cd_path="$(echo $(pwd)/${__cd_nxt} | sed "s;//;/;")";
-#                 echo $__cd_path;
-#                 echo;
-#                 ls -p --color=always "${__cd_path}";
-#         ')"
-#         [[ ${#dir} != 0 ]] || return 0
-#         builtin cd "$dir" &> /dev/null
-#     done
-# }
-
-#===============================================================================
-# 👇 https://github.com/xbin-io/xbin/
-#===============================================================================
-# % function xbin() {
-#   command="$1"
-#   shift
-#   args=( "$@" )
-#   if [ -t 0 ]; then
-#     curl -sS -X POST "https://xbin.io/${command}" -H "X-Args: ${args}"
-#   else
-#     curl -sS --data-binary @- "https://xbin.io/${command}" -H "X-Args: ${args}"
-#   fi
-# }
+cd() {
+  if [[ "$#" != 0 ]]; then
+    builtin cd "$@"
+    return
+  fi
+  while true; do
+    local lsd=$(echo ".." && ls -p | grep '/$' | sed 's;/$;;')
+    local dir="$(printf '%s\n' "${lsd[@]}" |
+      fzf --reverse --preview '
+                __cd_nxt="$(echo {})";
+                __cd_path="$(echo $(pwd)/${__cd_nxt} | sed "s;//;/;")";
+                echo $__cd_path;
+                echo;
+                ls -p --color=always "${__cd_path}";
+        ')"
+    [[ ${#dir} != 0 ]] || return 0
+    builtin cd "$dir" &>/dev/null
+  done
+}
 
 #===============================================================================
 # 👇 rip-venv
 #===============================================================================
 rip-venv() {
-    # Ensure a directory path is provided
-    if [[ -z "$1" ]]; then
-        echo "Error: No directory path provided. Please specify a path."
-        return 1
-    elif [[ ! -d "$1" ]]; then
-        echo "Error: The provided path is not a directory or does not exist."
-        return 1
-    fi
+  # Ensure a directory path is provided
+  if [[ -z "$1" ]]; then
+    echo "Error: No directory path provided. Please specify a path."
+    return 1
+  elif [[ ! -d "$1" ]]; then
+    echo "Error: The provided path is not a directory or does not exist."
+    return 1
+  fi
 
-    # Use fd to find .venv directories and store the results
-    local venv_dirs
-    venv_dirs=$(fd --type d --hidden --no-ignore ".venv" "$1" 2>/dev/null)
+  # Use fd to find .venv directories and store the results
+  local venv_dirs
+  venv_dirs=$(fd --type d --hidden --no-ignore ".venv" "$1" 2>/dev/null)
 
-    # Check if fd is not installed
-    if ! command -v fd &>/dev/null; then
-        echo "Error: 'fd' command is not installed. Please install fd to use this function."
-        return 1
-    fi
+  # Check if fd is not installed
+  if ! command -v fd &>/dev/null; then
+    echo "Error: 'fd' command is not installed. Please install fd to use this function."
+    return 1
+  fi
 
-    # Inform if no .venv directories are found
-    if [[ -z "$venv_dirs" ]]; then
-        echo "No .venv directories found."
-        return 0
-    fi
+  # Inform if no .venv directories are found
+  if [[ -z "$venv_dirs" ]]; then
+    echo "No .venv directories found."
+    return 0
+  fi
 
-    # Process each directory path
-    echo "Found .venv directories:"
-    echo "$venv_dirs"
-    echo "Proceed with deletion? [y/N]"
-    IFS= read -r proceed < /dev/tty
-    if [[ $proceed =~ ^[Yy]$ ]]; then
-        echo "$venv_dirs" | while IFS= read -r line; do
-            echo "Deleting $line..."
-            rip "$line"
-            echo "Deleted: $line"
-        done
-    else
-        echo "Deletion canceled."
-    fi
+  # Process each directory path
+  echo "Found .venv directories:"
+  echo "$venv_dirs"
+  echo "Proceed with deletion? [y/N]"
+  IFS= read -r proceed </dev/tty
+  if [[ $proceed =~ ^[Yy]$ ]]; then
+    echo "$venv_dirs" | while IFS= read -r line; do
+      echo "Deleting $line..."
+      rip "$line"
+      echo "Deleted: $line"
+    done
+  else
+    echo "Deletion canceled."
+  fi
 }
