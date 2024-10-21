e53bb87c848dbdaa9dc65a97dc89c236228a35e7
Alex <30626291+ipruning@users.noreply.github.com>
2024-10-21 15:13:31 +0800

    style: refactor keybindings for consistency and usability
    - Re-enable the `Ctrl g` binding to switch to the "Locked" mode in zellij.
    - Simplify keybindings for moving focus and tabs in zellij by removing alternative key combinations.
    - Change the `Ctrl g` and `Ctrl a` keybindings to `Ctrl h` and `Ctrl j` respectively for specific plugins in zellij.
    - Remove multiple custom keybindings related to vscode-neovim integration for VSCode.
    - Comment out previously removed keybindings in the VSCode keybindings JSON file.
    - Add Emacs-style motion keybindings for character and history navigation in the zsh configuration when using Zellij.
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- config/shell/env.zsh
+++ config/shell/env.zsh
@@ -168,6 +168,13 @@ zstyle ':fzf-tab:*' fzf-pad 10
 #===============================================================================
 # 👇 custom keybindings
 #===============================================================================
+# Option-F/B Emacs Motion
+if [[ -n "$ZELLIJ" ]]; then
+  bindkey "^F" forward-char
+  bindkey "^B" backward-char
+  bindkey "^P" up-line-or-history
+  bindkey "^N" down-line-or-history
+fi
 # Option-Left
 bindkey "^[[1;3C" forward-word
 # Option-Right
