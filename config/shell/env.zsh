da5f97cd11b59b018c1e020328dce72cb785f44b
Alex <30626291+ipruning@users.noreply.github.com>
2024-12-03 11:39:18 +0800

    chore: refactor package handling and cleanup unused code
    - Add "Tripsy" to the list of packages in `macos_setapp.txt`.
    - Comment out the `zellij_tab_name_update` function and its associated calls in `env.zsh`.
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- config/shell/env.zsh
+++ config/shell/env.zsh
@@ -76,29 +76,25 @@ fi
 # }
 # preexec_functions+=(zellij_pane_name_update)
 
-zellij_tab_name_update() {
-  if [[ -n $ZELLIJ ]]; then
-    local current_dir=$PWD
-    if [[ $current_dir == $HOME ]]; then
-      current_dir="~"
-    else
-      current_dir=${current_dir##*/}
-    fi
-    command nohup zellij action rename-tab $current_dir >/dev/null 2>&1
-  fi
-}
+# zellij_tab_name_update() {
+#   if [[ -n $ZELLIJ ]]; then
+#     local current_dir=$PWD
+#     if [[ $current_dir == $HOME ]]; then
+#       current_dir="~"
+#     else
+#       current_dir=${current_dir##*/}
+#     fi
+#     command nohup zellij action rename-tab $current_dir >/dev/null 2>&1
+#   fi
+# }
 
-zellij_tab_name_update
-chpwd_functions+=(zellij_tab_name_update)
+# zellij_tab_name_update
+# chpwd_functions+=(zellij_tab_name_update)
 
 #===============================================================================
 # 👇 zsh Theme
 #===============================================================================
-if [[ -n $SSH_CONNECTION ]]; then
-  eval "$(starship init zsh)"
-else
-  eval "$(starship init zsh)"
-fi
+eval "$(starship init zsh)"
 
 #===============================================================================
 # 👇 Fix Case Sensitivity
