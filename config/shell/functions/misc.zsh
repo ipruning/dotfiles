ba62267ad8d9f03ebcb06aa62a611f3c3c2661ae
Alex <30626291+ipruning@users.noreply.github.com>
2024-12-28 17:17:08 +0800

    feat: refactor and enhance shell utility scripts
    - Removed deprecated aliases and functions from `aliases.zsh`.
    - Added alias for `vim` to use `nvim` in `aliases.zsh`.
    - Moved several utility functions from `aliases.zsh` to `misc.zsh`.
    - Added functions for syncing GitHub forks, Fava, Homebrew updates and upgrades, backup, and generating shell completions in `misc.zsh`.
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- config/shell/functions/misc.zsh
+++ config/shell/functions/misc.zsh
@@ -128,3 +128,62 @@ function set_proxy() {
 function nunset_proxy() {
   unset https_proxy http_proxy all_proxy
 }
+
+#===============================================================================
+# 👇 gh-sync-fork
+#===============================================================================
+functiongh-sync-fork() {
+  gh repo list --fork --visibility public --json owner,name | jq -r 'map(.owner.login + "/" + .name) | .[]' | xargs -t -L1 gh repo sync
+}
+
+#===============================================================================
+# 👇 fava
+#===============================================================================
+function r-fava() {
+  fava ${HOME}/Databases/Ledger/main.bean -p 4000
+}
+
+#===============================================================================
+# 👇 upgrade / backup
+#===============================================================================
+function r-completion() {
+  echo -e "\033[33mGenerating completions...\033[0m"
+  rm -f ~/.zcompdump
+  compinit
+}
+
+function r-update() {
+  echo -e "\033[33mUpdating Homebrew...\033[0m"
+  brew update
+
+  echo -e "\033[33mUpdating tldr pages...\033[0m"
+  tldr --update
+}
+
+function r-upgrade() {
+  echo -e "\033[33mUpgrading Homebrew formulas...\033[0m"
+  brew upgrade
+
+  echo -e "\033[33mCleaning up Homebrew...\033[0m"
+  brew cleanup && brew autoremove
+
+  echo -e "\033[33mUpgrading GitHub CLI extensions...\033[0m"
+  gh extension upgrade --all
+
+  echo -e "\033[33mUpdating mise...\033[0m"
+  mise upgrade
+
+  echo -e "\033[33mUpdating rust...\033[0m"
+  rustup update && rustup self update
+}
+
+function r-backup() {
+  echo -e "\033[33mBacking up all packages...\033[0m"
+  brew bundle dump --file="$HOME"/dotfiles/config/packages/Brewfile --force
+  brew leaves >"$HOME"/dotfiles/config/packages/Brewfile.txt
+  brew update
+  cp "$HOME"/.zsh_history "$HOME"/Databases/Backup/CLI/zsh_history_$(date +\%Y_\%m_\%d_\%H_\%M_\%S).bak
+  gh extension list | awk '{print $3}' >"$HOME"/dotfiles/config/packages/gh_extensions.txt
+  ls /Applications | rg '\.app' | sed 's/\.app//g' >"$HOME"/dotfiles/config/packages/macos_applications.txt
+  ls /Applications/Setapp | rg '\.app' | sed 's/\.app//g' >"$HOME"/dotfiles/config/packages/macos_setapp.txt
+}
