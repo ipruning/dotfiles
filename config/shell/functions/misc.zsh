e5de42c61072dd019efce25e227f0a716ba3bfa8
Alex <30626291+ipruning@users.noreply.github.com>
2024-12-29 23:19:29 +0800

    feat: improve session management and pane updates in Zellij integration
    - Reintroduce and enable the `zellij_pane_name_update` function to update pane names in Zellij based on the current directory.
    - Add a check within the `zj` function to ensure Zellij is installed before attempting to attach or create a session.
    - Modify the `zj` function to directly attach to an existing session if specified or list sessions using fzf for selection.
    - Simplify and improve the session handling process in the `zj` function by attaching to a session or creating a new one if none is selected.
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- config/shell/functions/misc.zsh
+++ config/shell/functions/misc.zsh
@@ -1,100 +1,33 @@
-#===============================================================================
-# 👇 fzf Chrome 历史记录
-#===============================================================================
-# fch() {
-#   local cols sep google_history open
-#   cols=$((COLUMNS / 3))
-#   sep='{::}'
-
-#   if [ "$(uname)" = "Darwin" ]; then
-#     google_history="$HOME/Library/Application Support/Google/Chrome/Default/History"
-#     open=open
-#   else
-#     google_history="$HOME/.config/google-chrome/Default/History"
-#     open=xdg-open
-#   fi
-#   cp -f "$google_history" /tmp/h
-#   sqlite3 -separator $sep /tmp/h \
-#     "select substr(title, 1, $cols), url
-#     from urls order by last_visit_time desc" |
-#     awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
-#     fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs $open >/dev/null 2>/dev/null
-# }
-
-#===============================================================================
-# 👇 fzf Chrome 书签
-#===============================================================================
-# fcb() {
-#   bookmarks_path=~/Library/Application\ Support/Google/Chrome/Default/Bookmarks
-#   jq_script='
-#         def ancestors: while(. | length >= 2; del(.[-1,-2]));
-#         . as $in | paths(.url?) as $key | $in | getpath($key) | {name,url, path: [$key[0:-2] | ancestors as $a | $in | getpath($a) | .name?] | reverse | join("/") } | .path + "/" + .name + "\t" + .url'
-#   jq -r "$jq_script" <"$bookmarks_path" |
-#     sed -E $'s/(.*)\t(.*)/\\1\t\x1b[36m\\2\x1b[m/g' |
-#     fzf --ansi |
-#     cut -d$'\t' -f2 |
-#     xargs open
-# }
-
-#===============================================================================
-# 👇 fzf 杀进程
-#===============================================================================
-# fkill() {
-#   (
-#     date
-#     ps -ef
-#   ) |
-#     fzf --bind='ctrl-r:reload(date; ps -ef)' \
-#       --header=$'Press CTRL-R to reload\n\n' --header-lines=2 \
-#       --preview='echo {}' --preview-window=down,3,wrap \
-#       --layout=reverse --height=80% | awk '{print $2}' | xargs kill -9
-# }
-
-#===============================================================================
 # 👇 zellij
 #===============================================================================
-zj() {
-  local session=""
-
-  if [[ $# -eq 1 ]]; then
-    session="$1"
-  else
-    session=$(zellij list-sessions --no-formatting | awk '{
-          session_name=$1; $1="";
-          if ($0 ~ /EXITED/) print "\033[31m" session_name "\033[0m\t" $0;
-          else print "\033[32m" session_name "\033[0m\t" $0;
-      }' | column -t -s $'\t' | fzf --ansi --exit-0 --header="Select a session to attach (or press Esc to create new):" | awk '{print $1}')
+function zj() {
+  # Check if zellij is installed
+  if ! command -v zellij >/dev/null 2>&1; then
+    echo "Zellij is not installed. Please install it first."
+    return 1
   fi
 
-  if [[ -n "$session" ]]; then
-    zellij attach "$session"
+  # If session name is provided as argument, use it directly
+  if [[ $# -eq 1 ]]; then
+    zellij attach "$1" 2>/dev/null || zellij --session "$1"
+    return
+  fi
+
+  # List and format existing sessions
+  local session=$(zellij list-sessions --no-formatting | awk '{
+    session_name=$1; $1="";
+    if ($0 ~ /EXITED/) print "\033[31m" session_name "\033[0m\t" $0;
+    else print "\033[32m" session_name "\033[0m\t" $0;
+  }' | column -t -s $'\t' | fzf --ansi --exit-0 --header="Select a session to attach (or press Esc to create new):" | awk '{print $1}')
+
+  # If no session is selected or no sessions exist, create a new one
+  if [[ -z "$session" ]]; then
+    zellij
   else
-    echo "No session selected"
+    zellij attach "$session"
   fi
 }
-
-#===============================================================================
-# 👇 cd
-#===============================================================================
-# cd() {
-#   if [[ "$#" != 0 ]]; then
-#     builtin cd "$@"
-#     return
-#   fi
-#   while true; do
-#     local ls=$(echo ".." && ls -p | grep '/$' | sed 's;/$;;')
-#     local dir="$(printf '%s\n' "${ls[@]}" |
-#       fzf --reverse --preview '
-#                 __cd_nxt="$(echo {})";
-#                 __cd_path="$(echo $(pwd)/${__cd_nxt} | sed "s;//;/;")";
-#                 echo $__cd_path;
-#                 echo;
-#                 eza --icons --oneline --color=always --ignore-glob=".DS_Store" "${__cd_path}";
-#         ')"
-#     [[ ${#dir} != 0 ]] || return 0
-#     builtin cd "$dir" &>/dev/null
-#   done
-# }
+# 如果 "$ZELLIJ" 是 0 就代表已经在仓库里了
 
 #===============================================================================
 # 👇 Roam Research
