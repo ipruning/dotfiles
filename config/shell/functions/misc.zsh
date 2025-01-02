dc45399448434395d31dc234eb813dde09fc8a9b
Alex <30626291+ipruning@users.noreply.github.com>
2025-01-02 21:09:40 +0800

    feat: some functions

--- config/shell/functions/misc.zsh
+++ config/shell/functions/misc.zsh
@@ -1,3 +1,49 @@
+function r-fava() {
+  fava ${HOME}/Databases/Ledger/main.bean -p 4000
+}
+
+function r-completion() {
+  echo -e "\033[33mGenerating completions...\033[0m"
+  rm -f ~/.zcompdump
+  compinit
+}
+
+function r-update() {
+  echo -e "\033[33mUpdating Homebrew...\033[0m"
+  brew update
+
+  echo -e "\033[33mUpdating tldr pages...\033[0m"
+  tldr --update
+}
+
+function r-upgrade() {
+  echo -e "\033[33mUpgrading Homebrew formulas...\033[0m"
+  brew upgrade
+
+  echo -e "\033[33mCleaning up Homebrew...\033[0m"
+  brew cleanup && brew autoremove
+
+  echo -e "\033[33mUpgrading GitHub CLI extensions...\033[0m"
+  gh extension upgrade --all
+
+  echo -e "\033[33mUpdating mise...\033[0m"
+  mise upgrade --bump
+
+  echo -e "\033[33mUpdating rust...\033[0m"
+  rustup update && rustup self update
+}
+
+function r-backup() {
+  echo -e "\033[33mBacking up all packages...\033[0m"
+  brew bundle dump --file="$HOME"/dotfiles/config/packages/Brewfile --force
+  brew leaves >"$HOME"/dotfiles/config/packages/Brewfile.txt
+  brew update
+  cp "$HOME"/.zsh_history "$HOME"/Databases/Backup/CLI/zsh_history_$(date +\%Y_\%m_\%d_\%H_\%M_\%S).bak
+  gh extension list | awk '{print $3}' >"$HOME"/dotfiles/config/packages/gh_extensions.txt
+  ls /Applications | rg '\.app' | sed 's/\.app//g' >"$HOME"/dotfiles/config/packages/macos_applications.txt
+  ls /Applications/Setapp | rg '\.app' | sed 's/\.app//g' >"$HOME"/dotfiles/config/packages/macos_setapp.txt
+}
+
 function zj() {
   if ! command -v zellij >/dev/null 2>&1; then
     echo "zellij is not installed. please install it first."
@@ -74,111 +120,19 @@ function uproxy() {
   unset https_proxy http_proxy all_proxy
 }
 
-functiongh-sync-fork() {
+function sfork() {
   gh repo list --fork --visibility public --json owner,name | jq -r 'map(.owner.login + "/" + .name) | .[]' | xargs -t -L1 gh repo sync
 }
 
-function r-fava() {
-  fava ${HOME}/Databases/Ledger/main.bean -p 4000
-}
-
-function r-completion() {
-  echo -e "\033[33mGenerating completions...\033[0m"
-  rm -f ~/.zcompdump
-  compinit
-}
-
-function r-update() {
-  echo -e "\033[33mUpdating Homebrew...\033[0m"
-  brew update
-
-  echo -e "\033[33mUpdating tldr pages...\033[0m"
-  tldr --update
-}
-
-function r-upgrade() {
-  echo -e "\033[33mUpgrading Homebrew formulas...\033[0m"
-  brew upgrade
-
-  echo -e "\033[33mCleaning up Homebrew...\033[0m"
-  brew cleanup && brew autoremove
-
-  echo -e "\033[33mUpgrading GitHub CLI extensions...\033[0m"
-  gh extension upgrade --all
-
-  echo -e "\033[33mUpdating mise...\033[0m"
-  mise upgrade --bump
-
-  echo -e "\033[33mUpdating rust...\033[0m"
-  rustup update && rustup self update
-}
-
-function r-backup() {
-  echo -e "\033[33mBacking up all packages...\033[0m"
-  brew bundle dump --file="$HOME"/dotfiles/config/packages/Brewfile --force
-  brew leaves >"$HOME"/dotfiles/config/packages/Brewfile.txt
-  brew update
-  cp "$HOME"/.zsh_history "$HOME"/Databases/Backup/CLI/zsh_history_$(date +\%Y_\%m_\%d_\%H_\%M_\%S).bak
-  gh extension list | awk '{print $3}' >"$HOME"/dotfiles/config/packages/gh_extensions.txt
-  ls /Applications | rg '\.app' | sed 's/\.app//g' >"$HOME"/dotfiles/config/packages/macos_applications.txt
-  ls /Applications/Setapp | rg '\.app' | sed 's/\.app//g' >"$HOME"/dotfiles/config/packages/macos_setapp.txt
-}
-
-function rgist() {
+function ggist() {
   local filename="$1"
-  local command="${2:-uv}"  # Default to 'uv'
-  local command_args="${3:-run}"
-  local cachedir="${XDG_CACHE_HOME:-$HOME/.cache}/rgist"
-  local cachefile
-  local raw_url
-  local gist_id
 
   if [[ -z "$filename" ]]; then
-    echo "Usage: rgist <filename> [command] [command_args]"
-    echo "  command: The command to execute the downloaded file. Default is 'uv'."
-    echo "  command_args: The arguments for the command. Default is 'run'."
+    echo "Usage: getgist <filename>"
     return 1
   fi
 
-  # Extract gist_id from latest gist for caching purposes
-  gist_id=$(gh api \
-    -H "Accept: application/vnd.github+json" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    /gists | jq -r ".[0].id")
-
-  if [[ -z "$gist_id" ]]; then
-    echo "Error: Could not retrieve latest gist ID."
-    return 1
-  fi
-
-  # Set up caching filename based on gist id and the filename provided by the user
-  mkdir -p "$cachedir"
-  cachefile="$cachedir/${gist_id}_$(echo "$filename" | sed 's/[^a-zA-Z0-9_.]/_/g')"
-
-
-  # Check for cached file and avoid re-downloading if possible
-  if [[ -f "$cachefile" ]]; then
-    if [[ "$command" == "uv" ]] && [[ "$command_args" == "run" ]]; then
-        if [[ -n "$(find "$cachefile" -mmin +1440)" ]]; then
-        echo "Cache expired. Re-downloading file."
-      else
-          echo "Using cached version of '$filename'."
-          "$command" "$command_args" "$cachefile"
-          return 0
-      fi
-    else
-         echo "Using cached version of '$filename'."
-          if ! command -v "$command" &> /dev/null; then
-              echo "Error: Command '$command' not found in PATH."
-            return 1
-           fi
-       "$command" "$command_args" "$cachefile"
-       return 0
-    fi
-  fi
-
-  # Get the raw URL for the filename
-  raw_url=$(gh api \
+  local raw_url=$(gh api \
     -H "Accept: application/vnd.github+json" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     /gists | jq -r ".[] | .files | to_entries[] | select(.key == \"$filename\") | .value.raw_url" | head -n 1)
@@ -188,34 +142,21 @@ function rgist() {
     return 1
   fi
 
-  echo "Downloading '$filename' from Gist..."
-
-  # Download and cache the file
-  curl -sL "$raw_url" -o "$cachefile"
-
-  if ! command -v "$command" &> /dev/null; then
-     echo "Error: Command '$command' not found in PATH."
-    return 1
-  fi
-
-  "$command" "$command_args" "$cachefile"
+  echo "$raw_url"
 }
 
-function randomid() {
+function grandomid() {
   random_number() {
     echo $((100000 + RANDOM % 900000))
   }
 
-  # Generate ID
   generate_id() {
-    local prefix=$1  # Get prefix
-    local random_num=$(random_number) # Get random number
+    local prefix=$1
+    local random_num=$(random_number)
     echo "${prefix}${random_num}"
   }
 
-  # Get prefix (use default "id" if not provided)
   local prefix="${1:-id}"
 
-  # Generate and print ID
   generate_id "$prefix"
 }
