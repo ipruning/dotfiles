0a8ab63da25a6180bded4e9297f33b478449688d
Alex <30626291+ipruning@users.noreply.github.com>
2025-01-09 17:44:46 +0800

    feat: enhance shell configuration with new aliases and tool setups
    - Re-add aliases for navigating up directories, and add new ones for multiple levels up.
    - Rename alias `szshrc` to `szsh`.
    - Add new git status alias `gs`.
    - Use `bat` instead of `cat` using an alias.
    - Add safety interactive option aliases for `rm`, `cp`, and `mv`.
    - Add alias to create parent directories with `mkdir`.
    - Add human-readable output format aliases for `df` and `du`.
    - Comment out `starship` theme initialization in zsh configuration.
    - Update `zoxide` initialization with a custom command and adjust fzf settings.
    - Adjust fzf default command and add custom completion generation functions.
    - Add PATH modification for the new Mojo tool in shell environment configuration.
    - Add initialization block for `conda` in the zprofile configuration.
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- config/shell/aliases.bash
+++ config/shell/aliases.bash
@@ -1,9 +1,11 @@
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+
 alias x86_64='arch -x86_64 zsh --login'
 alias x86_64_run='arch -x86_64 zsh -c'
 alias brow='/usr/local/homebrew/bin/brew'
-
-alias ..='cd ..'
-alias ...='cd ../..'
 
 alias l='eza --icons --oneline'
 alias la='eza --icons --all --oneline'
@@ -11,9 +13,20 @@ alias ll='eza --icons --long --git --time-style=long-iso'
 alias lla='eza --icons --long --all --git --time-style=long-iso'
 alias lt='eza --icons --long --all --git --time-style=long-iso --tree --level=3'
 
-alias szshrc='source ~/.zshrc'
+alias szsh='source ~/.zshrc'
 
-alias vim='nvim'
+alias gs='git status'
 
 alias ip='curl -4 ip.sb'
 alias ipv6='curl -6 ip.sb'
+
+alias cat='bat'
+alias vim='nvim'
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+alias mkdir='mkdir -p'
+alias df='df -h'
+alias du='du -h'
--- config/shell/env.bash
+++ config/shell/env.bash
@@ -4,7 +4,7 @@ autoload -Uz compinit
 compinit -d ~/.zcompdump-"$ZSH_VERSION"
 
 # 👇 zsh Theme
-eval "$(starship init zsh)"
+# eval "$(starship init zsh)"
 
 # 👇 zsh options
 setopt NO_NOMATCH
@@ -29,6 +29,9 @@ export PATH="$HOME/dotfiles/bin:$PATH"
 # 👇 LM Studio CLI tool
 export PATH="$HOME/.cache/lm-studio/bin:$PATH"
 
+# 👇 Mojo
+export PATH="$HOME/.modular/bin:$PATH"
+
 # 👇 PostgreSQL
 export LDFLAGS="-L/opt/homebrew/opt/postgresql@17/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/postgresql@17/include"
@@ -38,31 +41,46 @@ export PATH="/opt/homebrew/opt/postgresql@17/bin:$PATH"
 source "$HOME/.orbstack/shell/init.zsh" 2>/dev/null || :
 
 # 👇 zoxide
-# z foo<tab> # shows the same completions as cd
-# z foo<space><tab> # shows interactive completions via zoxide
-eval "$(zoxide init zsh)"
+eval "$(zoxide init zsh --cmd j)"
 
 # 👇 mise
 eval "$(mise activate zsh)"
 
 # 👇 fzf
 # shellcheck disable=SC1090
-source <(fzf --zsh)
+FZF_CTRL_R_OPTS="" FZF_CTRL_T_COMMAND="" FZF_ALT_C_COMMAND="" source <(fzf --zsh)
 export FZF_COMPLETION_TRIGGER="jk"
-export FZF_DEFAULT_COMMAND="fd --type file --strip-cwd-prefix --hidden --follow --exclude .venv --exclude .git --exclude .DS_Store"
+export FZF_DEFAULT_COMMAND="fd --type file \
+--strip-cwd-prefix \
+--follow"
 export FZF_DEFAULT_OPTS=" \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
 --color=selected-bg:#45475a \
 --multi"
+_fzf_compgen_path() {
+    fd --type f --hidden --follow \
+       --follow \
+       --exclude .git \
+       --exclude .venv \
+       --exclude .DS_Store \
+       . "$1"
+}
+_fzf_compgen_dir() {
+    fd --type d --hidden --follow \
+       --follow \
+       --exclude .git \
+       --exclude .venv \
+       --exclude .DS_Store \
+       . "$1"
+}
 
 # 👇 plugins
 ZSH_PLUGINS_DIR="$HOME/dotfiles/config/shell/plugins"
 
 # 👇 fzf-tab
 source "$ZSH_PLUGINS_DIR"/fzf-tab/fzf-tab.plugin.zsh
-# zstyle ":fzf-tab:*" continuous-trigger "ctrl-y"
 
 # 👇 fast-syntax-highlighting https://github.com/catppuccin/zsh-fsh
 source "$ZSH_PLUGINS_DIR"/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
