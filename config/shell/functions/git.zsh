d847e5eb10801936570921475168b37a7f3b3fde
Alex <30626291+ipruning@users.noreply.github.com>
2025-01-24 19:49:24 +0800

    feat: enhance git command handling and improve shell integration
    - Enable zsh-autosuggestions plugin in env.zsh.
    - Combine multiple lines to declare and initialize git_commands associative array in git.zsh.
    - Add a 'help' subcommand to the git function to display usage and available subcommands.
    - Modify the command execution logic in the git function to use arrays for handling arguments.
    - Update the _g function to escape descriptions for command completion suggestions.
    - Adjust return logic in the _g function for better subcommand handling.
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- config/shell/functions/git.zsh
+++ config/shell/functions/git.zsh
@@ -1,24 +1,30 @@
-typeset -A git_commands
-git_commands=(
-  a 'git add --all'
+typeset -A git_commands=(
+  a  'git add --all'
   ac 'op run -- codegpt commit --no_confirm'
-  c 'git commit -m'
-  d 'git diff'
-  l 'git lg'
-  s 'git status'
+  c  'git commit -m'
+  d  'git diff'
+  lg 'git lg'
+  s  'git status'
 )
 
 g() {
   if [[ $# -eq 0 ]]; then
     git status
+  elif [[ $1 == "help" ]]; then
+    echo "Usage: g [subcommand]"
+    for key desc in ${(kv)git_commands}; do
+      printf "  %-3s = %s\n" "$key" "$desc"
+    done
+    return
   else
     local cmd=$1
     if (( ${+git_commands[$cmd]} )); then
+      shift
       if [[ $cmd == "c" ]]; then
-        shift
-        git commit -m "$*"
+        git commit -m "$@"
       else
-        eval ${git_commands[$cmd]}
+        local command=(${=git_commands[$cmd]})
+        "$command[@]" "$@"
       fi
     else
       git "$@"
@@ -31,15 +37,15 @@ compdef _g g
 function _g() {
   local -a subcommands
   for key desc in ${(kv)git_commands}; do
-    subcommands+=("$key:${desc#git }")
+    subcommands+=("$key:${(q)desc}")
   done
 
   if (( CURRENT == 2 )); then
     _describe -t commands 'g subcommands' subcommands
-    _git
+    return
   else
     shift words
-    (( CURRENT -- ))
+    (( CURRENT-- ))
     _git
   fi
 }
