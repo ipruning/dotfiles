f688fcb70d4ccaee6ebd8680f7c998fb06244a41
Alex <30626291+ipruning@users.noreply.github.com>
2025-01-31 02:42:23 +0800

    refactor: revamp shell aliases for improved usability and safety
    - Remove old aliases for navigating directories and various common commands.
    - Add organized sections with aliases for Linux and macOS commands.
    - Introduce 'preserve-root' flag to certain Linux aliases for enhanced safety.
    - Replace and refine previous aliases with versions that include color output and improved usability.
    - Add new aliases for commonly used tools like `lazydocker`, `lazygit`, and utilities like `ss` and `viddy`.
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- config/shell/aliases.zsh
+++ config/shell/aliases.zsh
@@ -1,14 +1,35 @@
-alias ..='cd ..'
-alias ...='cd ../..'
-alias brow='/usr/local/homebrew/bin/brew'
-alias n='nvim'
+# Linux Aliases Part 1
+alias chgrp="chgrp --preserve-root"
+alias chmod="chmod --preserve-root"
+alias chown="chown --preserve-root"
+alias cp="cp -i"
+alias df="df -h"
+alias du="du -h"
+alias egrep="egrep --color=auto"
+alias fgrep="fgrep --color=auto"
+alias free="free -h"
+alias grep="grep --color=auto"
+alias ls="ls --color=auto"
+alias mkdir="mkdir -p"
+alias mv="mv -i"
+alias ping="ping -c 5"
+alias rm="rm -i"
 
-alias cat='bat'
-alias cp='cp -i'
-alias df='df -h'
-alias du='du -h'
-alias ls='eza --all --git --group-directories-first --header --long --time-style long-iso'
-alias mkdir='mkdir -p'
-alias mv='mv -i'
-alias rg='rg --ignore-case'
-alias rm='rm -i'
+# Linux Aliases Part 2
+alias ...="cd ../.."
+alias ..="cd .."
+alias hg="history | grep"
+alias ll="ls -lAh"
+alias lzd="lazydocker"
+alias lzg="lazygit"
+alias ports="ss -tulpn"
+
+# macOS Aliases Part 1
+alias brow="/usr/local/homebrew/bin/brew"
+alias n="nvim"
+alias cat="bat"
+alias rg="rg --ignore-case"
+alias ls="eza"
+alias ll="eza --all --git --group-directories-first --header --long --time-style long-iso"
+alias ports="viddy --interval 1s 'lsof -i @127.0.0.1 | grep LISTEN'"
+alias ping="ping"
