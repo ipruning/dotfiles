398931ecc73000d28e90b4ef52ff6701acc87cc5
Alex <30626291+ipruning@users.noreply.github.com>
2025-02-02 04:59:15 +0800

    feat: 调整 Fuzzy Finder 交互设计
    fzf 官方有支持 ** 补全，我中间调整为了 jk，这样可以实现比如 ping jk <tab> 能交互式的选择服务器，当然 fzf 也支持 ctrl-t 作为交互式的输入。
    tv 给了另外一个方向，可以用极简的方式设计自己的补全命令且支持 fuzzy find 这样就替代了 ** 的功能，而且 fzf 官方其实也就做了 kill、ssh、export、unset 的支持。
    在终端中说白了补全就是在合适的上下文中填入信息，一般来说能补全文字或者文件夹就够了，如果你能补全更加详细的就更好，比如在 uv run 之后只会补全 python 文件或者是 gist 中的 python 文件，这个就是 fzf 没有那么好实现的？（可能 Skill Issue 🤣）
    不过这样还有一个功能是我缺少的就是在 tab 补全的时候能自动输入内容，其实理想应该是 ctrl-t 用 tv 实现一个通用补全，如果在输入命令到一半的时候会尝试截取 zsh-completion 的信息让你 fzf 的输入命令，但这个没有必要重复造轮子了，可以教给 fzf-tab 来接管。这样也合理，就是所有补全都是用 tab 来扩展，然后有自己设计的一些奇妙工具，就是之前可能用 jk 来实现的，则统一为 ctrl-t。
    最后为了视觉统一，我把输入框的位置挪到了屏幕上方而不是屏幕下方（不排除以后再换一次的可能性 🤣）

--- config/shell/env.zsh
+++ config/shell/env.zsh
@@ -1,86 +1,65 @@
+# 👇 zsh Theme
+eval "$(starship init zsh)"
+
 # 👇 plugins
 ZSH_PLUGINS_DIR="$HOME/dotfiles/config/shell/plugins"
+
+# 👇 fzf
+# The user interface of fzf is fully customizable with a large number of configuration options.
+# For a quick setup, you can start with one of the style presets — default, full, or minimal — using the --style option.
+export FZF_DEFAULT_OPTS=" \
+--style minimal \
+--reverse \
+--multi \
+--bind 'ctrl-y:accept' \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a"
+
+# 👇 fzf-tab
+source "$ZSH_PLUGINS_DIR"/fzf-tab/fzf-tab.plugin.zsh
+
+zstyle ':completion:*:descriptions' format '[%d]'
+zstyle ':completion:*' menu no
+zstyle ':fzf-tab:*' use-fzf-default-opts yes
+
+# 👇 tv
+_tv_search() {
+    emulate -L zsh
+    zle -I
+
+    local current_prompt
+    current_prompt=$LBUFFER
+
+    local output
+
+    output=$(tv --autocomplete-prompt "$current_prompt" $*)
+
+    zle reset-prompt
+
+    if [[ -n $output ]]; then
+        RBUFFER=""
+        LBUFFER=$current_prompt$output
+
+        # zle accept-line
+    fi
+}
+
+zle -N tv-search _tv_search
+
+bindkey '^T' tv-search
 
 # 👇 fast-syntax-highlighting
 source "$ZSH_PLUGINS_DIR"/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
 
-# 👇 zsh-autosuggestions
-# source "$ZSH_PLUGINS_DIR"/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-
-# 👇 zsh-autocomplete
-# source "$ZSH_PLUGINS_DIR"/zsh-autocomplete/zsh-autocomplete.plugin.zsh
-# zstyle ':autocomplete:*' add-space \
-#     executables aliases functions builtins reserved-words commands
-# bindkey -M emacs '^Y' .complete-word
-# bindkey -M menuselect '^Y' .complete-word
-
-# 👇 fzf
-# shellcheck disable=SC1090
-# FZF_CTRL_R_OPTS="" FZF_CTRL_T_COMMAND="" FZF_ALT_C_COMMAND="" source <(fzf --zsh)
-# export FZF_COMPLETION_TRIGGER="jk"
-# export FZF_DEFAULT_COMMAND="fd --type file \
-# --strip-cwd-prefix \
-# --follow"
-# export FZF_DEFAULT_OPTS=" \
-# --bind 'ctrl-y:accept' \
-# --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-# --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-# --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
-# --color=selected-bg:#45475a \
-# --multi"
-# _fzf_comprun() {
-#   local command=$1
-#   shift
-
-#   case "$command" in
-#     export|unset) fzf --preview-window=noborder --preview "eval 'echo \$'{}" "$@" ;;
-#     ssh)          fzf --preview-window=noborder --preview 'dig {}' "$@" ;;
-#     *)            fzf --preview-window=noborder --preview '' "$@" ;;
-#   esac
-# }
-# _fzf_compgen_path() {
-#   fd --type file \
-#   --hidden \
-#   --follow \
-#   --exclude .git \
-#   --exclude .venv \
-#   --exclude .DS_Store \
-#   . "$1"
-# }
-# _fzf_compgen_dir() {
-#   fd --type directory \
-#   --hidden \
-#   --follow \
-#   --exclude .git \
-#   --exclude .venv \
-#   --exclude .DS_Store \
-#   . "$1"
-# }
-# _fzf_complete_j() {
-#   _fzf_complete --reverse --prompt="fd> " -- "$@" < <(
-#     fd --type directory \
-#     --hidden \
-#     --follow \
-#     --exclude .git \
-#     --exclude .venv \
-#     --exclude .DS_Store \
-#     .
-#   )
-# }
-
-# 👇 fzf-tab
-# source "$ZSH_PLUGINS_DIR"/fzf-tab/fzf-tab.plugin.zsh
-# zstyle ':fzf-tab:*' fzf-bindings 'ctrl-y:accept'
-# zstyle ':fzf-tab:*' accept-line enter
-
-# 👇 zsh Theme
-eval "$(starship init zsh)"
-
 # 👇 zsh options
-# setopt NO_NOMATCH
-# setopt NO_NULL_GLOB
 setopt interactivecomments
 zstyle ":completion:*" matcher-list "m:{a-z}={A-Za-z}"
+
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey "^v" edit-command-line
 
 # 👇 My preferred editor for local and remote sessions
 export EDITOR="zed --wait"
@@ -95,11 +74,6 @@ bindkey "^D" delete-word
 bindkey "^E" end-of-line
 bindkey "^F" forward-char
 
-# 👇 Edit command line
-autoload -U edit-command-line
-zle -N edit-command-line
-bindkey "^v" edit-command-line
-
 # 👇 Custom paths
 export PATH="$HOME/dotfiles/bin:$PATH"
 source "$HOME/dotfiles/config/shell/functions/misc.zsh"
@@ -109,14 +83,8 @@ source "$HOME/dotfiles/config/shell/functions/surge.zsh"
 export PATH="$HOME/developer/localhost/prototypes/utils/bin:$PATH"
 export PATH="$HOME/developer/localhost/prototypes/utils/bash-scripts:$PATH"
 
-# 👇 LM Studio CLI tool
-export PATH="$HOME/.cache/lm-studio/bin:$PATH"
-
 # 👇 Mojo
 export PATH="$HOME/.modular/bin:$PATH"
-
-# 👇 Java
-export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 
 # 👇 OrbStack
 source "$HOME/.orbstack/shell/init.zsh" 2>/dev/null || :
@@ -124,61 +92,8 @@ source "$HOME/.orbstack/shell/init.zsh" 2>/dev/null || :
 # 👇 zoxide
 eval "$(zoxide init zsh --cmd j)"
 
-# 👇 mise
-eval "$(mise activate zsh)"
-
-# 👇 tv
-eval "$(tv init zsh)"
-
 # 👇 atuin
 eval "$(atuin init zsh)"
 
-# 👇 session utils
-# https://github.com/zellij-org/zellij/issues/2744
-# https://github.com/zellij-org/zellij/issues/3081#issuecomment-1904349853
-function jump_to_repo() {
-  local repo_path
-
-  repo_path=$(tv git-repos)
-  [[ -z "$repo_path" ]] && return
-  cd "${repo_path}"
-
-  local repo_name=$(basename "${repo_path}")
-
-  # if [[ -n "$ZELLIJ" ]]; then
-  #   zellij action rename-session "${repo_name}"
-  # fi
-}
-
-function jump_to_repo_with_zellij_session() {
-  if [[ -n "$ZELLIJ" ]]; then
-  else
-    local repo_path
-    repo_path=$(tv git-repos)
-    [[ -z "$repo_path" ]] && return
-    cd "${repo_path}"
-    local repo_name=$(basename "${repo_path}")
-    zellij attach "${repo_name}" 2>/dev/null || zellij --session "${repo_name}"
-  fi
-}
-
-function jump_to_zellij_session() {
-  if [[ -n "$ZELLIJ" ]]; then
-  else
-    if [[ "$TERM_PROGRAM" == "ghostty" ]]; then
-      zj_sessions=$(zellij list-sessions --no-formatting --short)
-      case $(echo "$zj_sessions" | grep -c '^.') in
-        0)
-          zellij
-          ;;
-        1)
-          zellij attach "$zj_sessions"
-          ;;
-        *)
-          selected_session=$(echo "$zj_sessions" | tv --no-preview) &&
-          [[ -n "$selected_session" ]] && zellij attach "$selected_session"
-          ;;
-      esac
-    fi
-  fi
-}
+# 👇 mise
+eval "$(mise activate zsh)"
