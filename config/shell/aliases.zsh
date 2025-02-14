0dd4e07e6eb3358ee98ed078bb324918b6757556
Alex <30626291+ipruning@users.noreply.github.com>
2025-02-14 10:58:47 +0800

    style: standardize naming conventions and update paths and settings
    - Removed alias `lzd` in `aliases.zsh`.
    - Added alias for `keyboardmaestro` in `aliases.zsh`.
    - Updated paths from `Developer/local` to `Developer/self` in `env.zsh` and `misc.zsh`.
    - Updated `source_command` paths from `local` to `self` in `custom_channels.toml`.
    - Changed key binding task names and actions in `keymap.json`, including renaming tasks and modifying bind contexts.
    - Changed feature names and theme settings in `settings.json`.
    - Renamed task labels in `tasks.json` to use snake_case format.
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- config/shell/aliases.zsh
+++ config/shell/aliases.zsh
@@ -17,12 +17,12 @@ alias ...="cd ../.."
 alias ..="cd .."
 alias hg="history | grep"
 alias ll="ls -lAh"
-alias lzd="lazydocker"
 alias ports="ss -tulpn"
 
 # macOS Aliases Part 1
 alias brow="/usr/local/homebrew/bin/brew"
 alias cat="bat --paging=never"
+alias keyboardmaestro='/Applications/Keyboard\ Maestro.app/Contents/MacOS/keyboardmaestro'
 alias ll="eza --all --git --group-directories-first --header --long --time-style long-iso"
 alias n="nvim"
 alias ports="viddy --interval 1s 'lsof -i @127.0.0.1 | grep LISTEN'"
