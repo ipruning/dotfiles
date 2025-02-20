cad0eb0b9a44fa230b764d20a270efdf19082cfc
Alex <30626291+ipruning@users.noreply.github.com>
2025-02-21 03:18:09 +0800

    style: adjust user interface settings and configurations
    - Update FZF color scheme in `env.zsh`.
    - Increase UI scale in `config.toml` from 95 to 100.
    - Comment out certain keybindings in `keymap.json`, deactivating them.
    - Comment out zellij session attachment logic in `.zprofile`.
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- config/shell/env.zsh
+++ config/shell/env.zsh
@@ -12,24 +12,26 @@ __TREE_IGNORE="-I '.git' -I '*.py[co]' -I '__pycache__' $__TREE_IGNORE"
 __FD_COMMAND="-L -H --no-ignore-vcs ${__TREE_IGNORE//-I/-E} $__FD_COMMAND"
 
 export FZF_DEFAULT_COMMAND="fd $__FD_COMMAND"
+
 export FZF_DEFAULT_OPTS=" \
 --style minimal \
 --reverse \
 --multi \
 --bind 'ctrl-y:accept' \
---color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
---color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
---color=marker:#7287fd,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39 \
---color=selected-bg:#bcc0cc"
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a"
+
 # export FZF_DEFAULT_OPTS=" \
 # --style minimal \
 # --reverse \
 # --multi \
 # --bind 'ctrl-y:accept' \
-# --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-# --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-# --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
-# --color=selected-bg:#45475a"
+# --color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
+# --color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
+# --color=marker:#7287fd,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39 \
+# --color=selected-bg:#bcc0cc"
 
 # 👇 fzf-tab
 source "$ZSH_PLUGINS_DIR"/fzf-tab/fzf-tab.plugin.zsh
