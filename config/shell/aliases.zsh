282afcb27d2a4542dfc67fe6a17ea7554d0d18ee
Alex <30626291+ipruning@users.noreply.github.com>
2025-03-08 03:11:47 +0800

    refactor: refactor and optimize shell aliases configuration
    - Remove forced count of packets from `ping` alias.
    - Add gprintf alias conditionally for non-darwin OS types.
    - Add new alias `lt` for a tree-like directory listing using eza.
    - Remove several macOS specific aliases: `brow`, `cat`, and `n`.
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- config/shell/aliases.zsh
+++ config/shell/aliases.zsh
@@ -9,8 +9,11 @@ alias grep="grep --color=auto"
 alias ls="ls --color=auto"
 alias mkdir="mkdir -p"
 alias mv="mv -i"
-alias ping="ping -c 5"
 alias rm="rm -i"
+
+if [[ "$OSTYPE" != "darwin"* ]]; then
+  alias gprintf="printf"
+fi
 
 # Linux Aliases Part 2
 alias ...="cd ../.."
@@ -20,11 +23,9 @@ alias ll="ls -lAh"
 alias ports="ss -tulpn"
 
 # macOS Aliases Part 1
-alias brow="/usr/local/homebrew/bin/brew"
 alias c="pbcopy"
-alias cat="bat --paging=never"
 alias keyboardmaestro='/Applications/Keyboard\ Maestro.app/Contents/MacOS/keyboardmaestro'
 alias ll="eza --all --git --group-directories-first --header --long --time-style long-iso"
-alias n="nvim"
+alias lt="eza --all --git --group-directories-first --header --long --time-style long-iso --tree"
 alias p="pbpaste"
 alias ports="viddy --interval 1s 'lsof -i @127.0.0.1 | grep LISTEN'"
