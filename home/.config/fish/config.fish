13212abdde7fe74d849dcde9d40bac4712267cb4
Alex <30626291+ipruning@users.noreply.github.com>
2025-03-10 03:57:54 +0800

    chore: enhance Fish shell integration and configuration
    - Configure Fish shell to source Homebrew, Starship, Zoxide, TV, and Atuin during interactive sessions.
    - Add a custom function `y` for the Yazi file manager to change directories based on a temporary file.
    - Introduce several aliases for Linux and macOS environments, including commands for directory navigation and file operations.
    - Customize Fish shell colors and add configuration for Catppuccin themes with updated color variables.
    - Include Fisher plugins in the Fish universal variables file and update Fisher-related settings.
    - Set the default shell command for the Ghostty terminal configuration to Fish in login and interactive mode.
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,6 +1,53 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+if test -d /opt/homebrew
+    /opt/homebrew/bin/brew shellenv | source
 end
 
-starship init fish | source
-atuin init fish --disable-up-arrow | source
+if status is-interactive
+    set -g fish_greeting
+
+    starship init fish | source
+
+    zoxide init fish --cmd j | source
+
+    tv init fish | source
+    
+    atuin init fish --disable-up-arrow | source
+
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        yazi $argv --cwd-file="$tmp"
+        if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
+
+    # Linux Aliases Part 1
+    alias cp="cp -i"
+    alias df="df -h"
+    alias du="du -h"
+    alias egrep="egrep --color=auto"
+    alias fgrep="fgrep --color=auto"
+    alias free="free -h"
+    alias grep="grep --color=auto"
+    alias ls="ls --color=auto"
+    alias mkdir="mkdir -p"
+    alias mv="mv -i"
+    alias rm="rm -i"
+
+    # Linux Aliases Part 2
+    alias ...="cd ../.."
+    alias ..="cd .."
+    alias hg="history | grep"
+    alias ll="ls -lAh"
+    alias ports="ss -tulpn"
+
+    # macOS Aliases Part 1
+    alias c="pbcopy"
+    alias keyboardmaestro="/Applications/Keyboard\ Maestro.app/Contents/MacOS/keyboardmaestro"
+    alias ll="eza --all --git --group-directories-first --header --long --time-style long-iso"
+    alias lt="eza --all --git --group-directories-first --header --long --time-style long-iso --tree"
+    alias o="open ."
+    alias p="pbpaste"
+    alias ports="viddy --interval 1s 'lsof -i @127.0.0.1 | grep LISTEN'"
+end
