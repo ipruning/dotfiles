e9f2c9696f5f187e6142c9581eb433a73a1e6526
Alex <30626291+ipruning@users.noreply.github.com>
2025-04-05 16:23:21 +0800

    feat: enhance configuration with new tools and improved readability
    - Replace `git` with `starship` in the setup configuration.
    - Split the configuration for `relative-motions` to a multiline format for better readability.
    - Add configuration for `duckdb` with specific options.
    - Remove and replace some keymap definitions; add new keymaps for various actions including navigating to Downloads, yanking files, toggling panes, mounting, and diffing files.
    - Update plugin dependencies to new revisions, and add several new plugins including `duckdb`, `starship`, `mount`, `diff`, and `ouch`.
    - Include a dark theme flavor option (commented out) in the theme configuration.
    - Adjust manager ratio settings and incorporate a list of previewers and preloaders using `ouch` and `duckdb` for various file types.
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- home/.config/yazi/init.lua
+++ home/.config/yazi/init.lua
@@ -1,3 +1,13 @@
-require("git"):setup()
+require("starship"):setup()
 
-require("relative-motions"):setup({ show_numbers = "relative", show_motion = true, enter_mode = "first" })
+require("relative-motions"):setup({
+    show_numbers = "relative",
+    show_motion = true,
+    enter_mode = "first"
+})
+
+require("duckdb"):setup({
+    mode = "standard",
+    row_id = true,
+    minmax_column_width = 30
+})
