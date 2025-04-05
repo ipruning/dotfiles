037a64862596066ca7dc12f200421cf3378a660d
Alex <30626291+ipruning@users.noreply.github.com>
2025-04-05 19:06:26 +0800

    build: revamp plugin setup and key mappings configuration
    - Remove the setup for the "relative-motions" plugin from the Lua configuration.
    - Comment out the key mappings for "relative-motions" in the TOML configuration.
    - Introduce new key mappings for plugins "zoxide," "fzf," and others in the TOML configuration.
    - Update and add several plugin dependencies to the TOML package configuration, including "smart-enter," "toggle-pane," "mount," "diff," and "path-from-root."
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- home/.config/yazi/init.lua
+++ home/.config/yazi/init.lua
@@ -1,11 +1,5 @@
 require("starship"):setup()
 
-require("relative-motions"):setup({
-    show_numbers = "relative",
-    show_motion = true,
-    enter_mode = "first"
-})
-
 require("duckdb"):setup({
     mode = "standard",
     row_id = true,
