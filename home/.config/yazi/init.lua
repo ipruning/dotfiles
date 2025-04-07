9cd00354fae0bf1eb4a5d6196072eb6f0e128b4d
Alex <30626291+ipruning@users.noreply.github.com>
2025-04-08 03:16:01 +0800

    build: enhance plugin integration and configuration setup
    - Add the setup for the `full-border` and `relative-motions` plugins in the Lua configuration.
    - Reinstate key mappings for the `relative-motions` plugin in the TOML keymap configuration.
    - Update versions and hashes for the `relative-motions` and `duckdb` plugins in the package configuration.
    - Modify the ratio configuration from `[3, 4, 5]` to `[1, 1, 2]` in the main TOML configuration.
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- home/.config/yazi/init.lua
+++ home/.config/yazi/init.lua
@@ -1,7 +1,18 @@
+require("full-border"):setup {
+    ---@diagnostic disable-next-line: undefined-global
+    type = ui.Border.ROUNDED,
+}
+
 require("starship"):setup()
 
 require("duckdb"):setup({
     mode = "standard",
     row_id = true,
     minmax_column_width = 30
+})
+
+require("relative-motions"):setup({
+    show_numbers = "relative",
+    show_motion = true,
+    enter_mode = "first",
 })
