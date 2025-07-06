9deca0fa9a7bd236f15a19b3b9df21d0c3daa9bd
Alex <30626291+ipruning@users.noreply.github.com>
2025-07-06 22:22:11 +0800

    chore: refine init script and update .gitignore for plugins management
    - Update the .gitignore file to include specific directories for backups, completions, and plugins
    - Add paths for Zellij and fish completions to the .gitignore
    - Modify init.sh to create necessary directories and download Zellij plugin binaries
    - Enhance init.sh to ensure specific shell plugins are cloned if not already present
    - Improve the script's functionality for updating repositories in a structured format
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- .mise/tasks/init.sh
+++ .mise/tasks/init.sh
@@ -15,3 +15,23 @@ mise cfg
 mise tasks
 
 find "$(git rev-parse --show-toplevel)"/.mise/tasks/ -type f -exec chmod +x {} \;
+
+printf '\n\033[1;34m▶ Updating Zellij Plugins %s\033[0m\n'
+mkdir -p home/.config/zellij/plugins/
+curl -L -o home/.config/zellij/plugins/zjstatus.wasm https://github.com/dj95/zjstatus/releases/latest/download/zjstatus.wasm
+curl -L -o home/.config/zellij/plugins/zellij-sessionizer.wasm https://github.com/cunialino/zellij-sessionizer/releases/latest/download/sessionizer.wasm
+
+printf '\n\033[1;34m▶ Updating ZSH Plugins %s\033[0m\n'
+
+[ -d config/shell/plugins/fzf-tab ]                  || git clone --depth=1 https://github.com/Aloxaf/fzf-tab                             config/shell/plugins/fzf-tab
+[ -d config/shell/plugins/ugit ]                     || git clone --depth=1 https://github.com/Bhupesh-V/ugit.git                         config/shell/plugins/ugit
+[ -d config/shell/plugins/zsh-autocomplete ]         || git clone --depth=1 https://github.com/marlonrichert/zsh-autocomplete             config/shell/plugins/zsh-autocomplete
+[ -d config/shell/plugins/zsh-autosuggestions ]      || git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions              config/shell/plugins/zsh-autosuggestions
+[ -d config/shell/plugins/fast-syntax-highlighting ] || git clone --depth=1 https://github.com/zdharma-continuum/fast-syntax-highlighting config/shell/plugins/fast-syntax-highlighting
+
+gfold -d json "$@" 2>/dev/null |
+  jq -r '.[] | (.parent | rtrimstr("/")) + "/" + .name' |
+  while read -r repo; do
+      printf '\n\033[1;34m▶ Updating %s\033[0m\n' "$repo"
+      git -C "$repo" pull --ff-only
+  done
