bdc6b939efbda260d2382ea8f6cba4284b3c7cc1
Alex <30626291+ipruning@users.noreply.github.com>
2025-08-02 01:14:55 +0800

    chore: remove LM Studio CLI path addition from fish config on macOS
    - Remove LM Studio CLI path addition from the fish shell configuration on Darwin systems
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,7 +1,3 @@
 if test (uname) = Darwin
   source "$HOME/dotfiles/config/shell/env.fish"
 end
-
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH /Users/alex/.cache/lm-studio/bin
-# End of LM Studio CLI section
