2cfeb4c685dfb9dea7bbd0d0ee60c625b147adcc
Alex <30626291+ipruning@users.noreply.github.com>
2025-11-29 17:45:20 +0800

    feat: add restore task

---
+++ .mise/tasks/restore.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#MISE description="Restore your dotfiles"
+
+set -euo pipefail
+
+cd "$(git rev-parse --show-toplevel)" || exit 1
+
+command -v git >/dev/null 2>&1 || exit 1
+command -v gum >/dev/null 2>&1 || exit 1
+command -v mise >/dev/null 2>&1 || exit 1
+command -v uv >/dev/null 2>&1 || exit 1
+
+gum confirm "Are you sure you want to run mackup restore(force)?" && uvx mackup restore --force
