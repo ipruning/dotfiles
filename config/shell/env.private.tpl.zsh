215e9182a4b3f26ebcf4b491af928cc0f3b23981
Alex <30626291+ipruning@users.noreply.github.com>
2025-12-28 21:12:08 +0800

    chore(shell): remove tigrisfs function from private env

--- config/shell/env.private.tpl.zsh
+++ config/shell/env.private.tpl.zsh
@@ -1,10 +1,1 @@
 export HF_TOKEN="op://dev/Hugging Face/macos/HF_TOKEN"
-
-tigrisfs() {
-  env AWS_ACCESS_KEY_ID="op://dev/Tigris/alex-macbook-tigrisfs/ACCESS_KEY_ID" \
-      AWS_SECRET_ACCESS_KEY="op://dev/Tigris/alex-macbook-tigrisfs/SECRET_ACCESS_KEY" \
-      AWS_ENDPOINT_URL_S3="https://t3.storage.dev" \
-      AWS_ENDPOINT_URL_IAM="https://iam.storage.dev" \
-      AWS_REGION="auto" \
-      command tigrisfs "$@"
-}
