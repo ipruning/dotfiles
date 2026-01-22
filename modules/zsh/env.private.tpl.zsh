b217a4ca7535cfe0fe0b05ebfdf449e2d9c4f70b
Alex <30626291+ipruning@users.noreply.github.com>
2026-01-22 13:46:07 +0800

    chore(zsh): update config

--- modules/zsh/env.private.tpl.zsh
+++ modules/zsh/env.private.tpl.zsh
@@ -1,4 +1,4 @@
-# https://linear.app/settings/api-keys
+# https://linear.app/settings/account/security
 export LINEAR_API_KEY=""
 # https://github.com/settings/tokens/new?description=MISE_GITHUB_TOKEN
 export MISE_GITHUB_TOKEN=""
