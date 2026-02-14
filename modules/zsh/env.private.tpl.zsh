98a7cc12af9a116cba5387fc2104065bceb0b518
Alex <30626291+ipruning@users.noreply.github.com>
2026-02-14 15:39:24 +0800

    chore(zsh): store secrets as 1Password references

--- modules/zsh/env.private.tpl.zsh
+++ modules/zsh/env.private.tpl.zsh
@@ -1,4 +1,4 @@
 # https://linear.app/settings/account/security
-export LINEAR_API_KEY=""
+export LINEAR_API_KEY="op://jihuanshe/Linear/LINEAR_API_KEY"
 # https://github.com/settings/tokens/new?description=MISE_GITHUB_TOKEN
-export MISE_GITHUB_TOKEN=""
+export MISE_GITHUB_TOKEN="op://jihuanshe/GitHub/MISE_GITHUB_TOKEN"
