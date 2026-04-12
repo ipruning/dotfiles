64abf14997763254b28c0c806f9664565b0ace43
Alex <30626291+ipruning@users.noreply.github.com>
2026-04-13 03:59:28 +0800

    chore(zsh): add brrr secret template

--- modules/zsh/env.private.tpl.zsh
+++ modules/zsh/env.private.tpl.zsh
@@ -2,3 +2,5 @@
 export LINEAR_API_KEY="op://jihuanshe/Linear/LINEAR_API_KEY"
 # https://github.com/settings/tokens/new?description=MISE_GITHUB_TOKEN
 export MISE_GITHUB_TOKEN="op://jihuanshe/GitHub/MISE_GITHUB_TOKEN"
+# https://brrr.now/docs
+export BRRR_SECRET="op://Private/brrr/BRRR_SECRET"
